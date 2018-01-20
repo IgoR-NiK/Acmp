636b824aff1f91ef17a2d055f5c8186b651281d3
Igor Nikolaev <Igor_2501@mail.ru>
2018-01-20 10:09:47 +0300

    Решена задача 3 - "Пятью пять - двадцать пять" (Время: 1 сек. Память: 16 Мб Сложность: 8%)

--- TasksApp.Test/Tests/Task0003Test.cs
+++ TasksApp.Test/Tests/Task0003Test.cs
@@ -5,30 +5,30 @@ using TasksApp.Tasks;
 namespace TasksApp.Test.Tests
 {
     [TestClass]
-    public class Task0002Test
+    public class Task0003Test
     {
-        private void CheckTest(int n, long result)
+        private void CheckTest(int a, long result)
         {
-            var resultTest = Task0002.Solve(n);
-            Assert.AreEqual(resultTest, result);            
+            var resultTest = Task0003.Solve(a);
+            Assert.AreEqual(resultTest, result);                 
         }
 
         [TestMethod]
         public void Test1()
         {
-            CheckTest(5, 15);
+            CheckTest(5, 25);
         }
 
         [TestMethod]
         public void Test2()
         {
-            CheckTest(9, 45);
+            CheckTest(75, 5625);
         }
 
         [TestMethod]
         public void Test3()
         {
-            CheckTest(-4, -9);
+            CheckTest(4255, 18105025);
         }
     }
 }
