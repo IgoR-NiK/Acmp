9210a53710d994f3738a151f099d8ec858834115
Igor Nikolaev <Igor_2501@mail.ru>
2018-01-20 11:07:58 +0300

    Решена задача 5 - "Статистика" (Время: 1 сек. Память: 16 Мб Сложность: 15%)

---
+++ TasksApp.Test/Tests/Task0005Test.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0005Test
+    {
+        private void CheckTest(Task0005.Output result, string answer, int n, params int[] days)
+        {
+            var resultTest = Task0005.Solve(n, days);
+            
+            for(var i = 0; i < resultTest.OddDays.Count; i++)            
+                Assert.AreEqual(resultTest.OddDays[i], result.OddDays[i]);
+
+            for (var i = 0; i < resultTest.EvenDays.Count; i++)
+                Assert.AreEqual(resultTest.EvenDays[i], result.EvenDays[i]);
+
+            Assert.AreEqual(resultTest.Answer, answer);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Task0005.Output output = new Task0005.Output()
+            {
+                EvenDays = new List<int>() { 4, 16, 2 },
+                OddDays = new List<int>() { 19, 31 }
+            };
+            CheckTest(output, "YES", 5, 4, 16, 19, 31, 2);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Task0005.Output output = new Task0005.Output()
+            {
+                EvenDays = new List<int>() { 4, 12, 24 },
+                OddDays = new List<int>() { 29, 7, 15, 17, 1 }
+            };
+            CheckTest(output, "NO", 8, 29, 4, 7, 12, 15, 17, 24, 1);
+        }
+    }
+}
---
+++ TasksApp/Tasks/Task0005.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 5 - "Статистика"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 15%)
+    /// 
+    /// Вася не любит английский язык, но каждый раз старается получить хотя бы четверку за четверть, чтобы оставаться ударником. 
+    /// В текущей четверти Вася заметил следующую закономерность: по нечетным дням месяца он получал тройки, а по четным – четверки. 
+    /// Так же он помнит, в какие дни он получал эти оценки. Поэтому он выписал на бумажке все эти дни для того, чтобы оценить, 
+    /// сколько у него троек и сколько четверок. Помогите Васе это сделать, расположив четные и нечетные числа в разных строчках. 
+    /// Вася может рассчитывать на оценку 4, если четверок не меньше, чем троек.
+    /// 
+    /// Входные данные
+    /// В первой строке входного файла INPUT.TXT записано единственное число N – количество элементов целочисленного массива (1 ≤ N ≤ 100). 
+    /// Вторая строка содержит N чисел, представляющих заданный массив. Каждый элемент массива – натуральное число от 1 до 31. 
+    /// Все элементы массива разделены пробелом. 
+    /// 
+    /// Выходные данные
+    /// В первую строку выходного файла OUTPUT.TXT нужно вывести числа, которые соответствуют дням месяцев, в которые Вася получил тройки, 
+    /// а во второй строке соответственно расположить числа месяца, в которые Вася получил четверки. В третьей строке нужно вывести «YES», 
+    /// если Вася может рассчитывать на четверку и «NO» в противном случае. В каждой строчке числа следует выводить в том же порядке, 
+    /// в котором они идут во входных данных. При выводе, числа отделяются пробелом.
+    /// </summary>
+    public class Task0005
+    {
+        public static void Main()
+        {
+            int n;
+            int[] days;
+            GetInputData(out n, out days);
+
+            var result = Solve(n, days);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int n, out int[] days)
+        {            
+            n = int.Parse(Console.ReadLine());            
+            string[] str = Console.ReadLine().Split();
+
+            days = new int[n];
+            for (var i = 0; i < str.Length; i++)
+                days[i] = int.Parse(str[i]);                
+        }
+
+        public static Output Solve(int n, int[] days)
+        {
+            Output output = new Output();
+
+            foreach (var day in days)
+            {
+                switch(day % 2)
+                {
+                    case 0:
+                        output.EvenDays.Add(day);
+                        break;
+                    case 1:
+                        output.OddDays.Add(day);
+                        break;
+                }
+            }
+
+            return output;
+        }
+
+        private static void PrintResult(Output result)
+        {
+            Console.WriteLine(result);
+        }
+
+        public class Output
+        {
+            /// <summary>
+            /// Нечётные дни
+            /// </summary>
+            public List<int> OddDays = new List<int>();
+
+            /// <summary>
+            /// Чётные дни
+            /// </summary>
+            public List<int> EvenDays = new List<int>();
+
+            /// <summary>
+            /// Ответ на задачу
+            /// </summary>
+            public string Answer
+            {
+                get
+                {
+                    return (OddDays.Count > EvenDays.Count) ? "NO" : "YES";
+                }
+            }  
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach(var day in OddDays)
+                {
+                    sb.Append(day);
+                    sb.Append(' ');
+                }
+                sb.Append(Environment.NewLine);
+
+                foreach (var day in EvenDays)
+                {
+                    sb.Append(day);
+                    sb.Append(' ');
+                }
+                sb.Append(Environment.NewLine);
+                
+                sb.Append(Answer);
+
+                return sb.ToString();
+            }
+
+        }
+    }
+}
