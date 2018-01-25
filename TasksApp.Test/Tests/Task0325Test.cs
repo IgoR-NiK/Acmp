53241b8d044402c644adb0050fe8f9985794948d
Igor Nikolaev <Igor_2501@mail.ru>
2018-01-25 20:27:48 +0300

    Решена задача 325 - "Мы с конем вдвоем по полю пойдем" (Время: 1 сек. Память: 16 Мб Сложность: 31%)

---
+++ TasksApp.Test/Tests/Task0325Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0325Test
+    {
+        private void CheckTest(string firstCoordinate, string secondCoordinate, string output)
+        {
+            var test = Task0325.Solve(firstCoordinate, secondCoordinate);
+            Assert.AreEqual(test, output);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest("a1", "h8", "NO");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest("a1", "b3", "1");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            CheckTest("a1", "d4", "2");
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            CheckTest("c4", "c4", "2");
+        }
+    }
+}
---
+++ TasksApp/Tasks/Task0325.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 325 - "Мы с конем вдвоем по полю пойдем"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 31%)
+    /// 
+    /// Заданы две клетки шахматной доски. Требуется определить, возможно ли попасть из одной клетки в другую одним ходом шахматного коня, 
+    /// а если нет, то следует выяснить, возможно ли попасть с помощью двух ходов.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит координаты двух клеток в общепринятом формате: каждая координата записывается 
+    /// как английская строчная буква и цифра, координаты отделены друг от друга запятой и пробелом. 
+    /// 
+    /// Выходные данные
+    /// Выходной файл OUTPUT.TXT должен содержать цифру «1», если возможно из одной клетки в другую попасть за 1 ход, 
+    /// либо цифру «2», если попасть можно за 2 хода, либо «NO», если одна клетка недостижима из другой ни за 1 ни за 2 хода.
+    /// </summary>
+    public class Task0325
+    {
+        public static void Main()
+        {
+            string sourceCell, terminalCell;
+            GetInputData(out sourceCell, out terminalCell);
+
+            var result = Solve(sourceCell, terminalCell);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out string sourceCell, out string terminalCell)
+        {
+            var input = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            sourceCell = input[0];
+            terminalCell = input[1];
+        }
+
+        public static string Solve(string sourceCell, string terminalCell)
+        {
+            // Находим клетки после первого хода
+            var firstMoves = GetAvailableMovesFrom(sourceCell);
+
+            // Находим клетки после второго хода
+            var secondMoves = new List<string>();
+            firstMoves.ForEach((firstMove) => secondMoves.AddRange(GetAvailableMovesFrom(firstMove)));
+
+            return firstMoves.Contains(terminalCell) ? "1" : secondMoves.Contains(terminalCell) ? "2" : "NO";
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+
+        private static List<string> GetAvailableMovesFrom(string cell)
+        {
+            var availableMoves = new List<string>();
+
+            var row = cell[1] - '1';
+            var column = cell[0] - 'a';
+
+            if (column + 1 < 8 && row - 2 >= 0)
+                availableMoves.Add(getCoordinate(column + 1, row - 2));
+            if (column + 2 < 8 && row - 1 >= 0)
+                availableMoves.Add(getCoordinate(column + 2, row - 1));
+            if (column + 2 < 8 && row + 1 < 8)
+                availableMoves.Add(getCoordinate(column + 2, row + 1));
+            if (column + 1 < 8 && row + 2 < 8)
+                availableMoves.Add(getCoordinate(column + 1, row + 2));
+            if (column - 1 >= 0 && row + 2 < 8)
+                availableMoves.Add(getCoordinate(column - 1, row + 2));
+            if (column - 2 >= 0 && row + 1 < 8)
+                availableMoves.Add(getCoordinate(column - 2, row + 1));
+            if (column - 2 >= 0 && row - 1 >= 0)
+                availableMoves.Add(getCoordinate(column - 2, row - 1));
+            if (column - 1 >= 0 && row - 2 >= 0)
+                availableMoves.Add(getCoordinate(column - 1, row - 2));
+
+            return availableMoves;
+        }
+
+        private static string getCoordinate(int column, int row)
+        {
+            return String.Format("{0}{1}", (char)('a' + column), (char)('1' + row));
+        }
+    }
+}
