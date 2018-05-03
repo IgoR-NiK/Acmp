cc73dbd59f99095774eebfa4554162592bdc9199
Igor Nikolaev <Igor_2501@mail.ru>
2018-05-03 21:32:32 +0300

    Решена задача 8 - "Арифметика" (Время: 1 сек. Память: 16 Мб Сложность: 5%)

---
+++ TasksApp.Test/Tests/Task0008Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0008Test
+    {
+        private void CheckTest(int a, int b, int c, string output)
+        {
+            var test = Task0008.Solve(a, b, c);
+            Assert.AreEqual(test, output);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(8, 54, 432, "YES");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(16, 19, 777, "NO");
+        }
+    }
+}
