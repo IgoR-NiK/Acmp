7d01468b10f075c2d3889b9fbcd1a391e1c802bd
Igor Nikolaev <Igor_2501@mail.ru>
2018-06-29 02:43:44 +0300

    Решена задача 22 - "Единицы" (Время: 1 сек. Память: 16 Мб Сложность: 16%)

---
+++ CSharp/TasksApp.Test/Tests/Task0022Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0022Test
+    {
+        private void CheckTest(int n, int output)
+        {
+            var test = Task0022.Solve(n);
+            Assert.AreEqual(test, output);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(5, 2);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(7, 3);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0022.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 22 - "Единицы"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 16%)
+    /// 
+    /// На уроках информатики вас, наверное, учили переводить числа из одних систем счисления в другие и выполнять другие подобные операции. 
+    /// Пришло время продемонстрировать эти знания. Найдите количество единиц в двоичной записи заданного числа.
+    /// 
+    /// Входные данные
+    /// Во входном файле INPUT.TXT записано целое число n (0 ≤ n ≤ 2*10^9).
+    /// 
+    /// Выходные данные
+    /// В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — количество двоичных единиц в записи числа n.
+    /// </summary>
+    public class Task0022
+    {
+        public static void Main()
+        {
+            int n;
+            GetInputData(out n);
+
+            var result = Solve(n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+            var s = 0;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    s++;
+                }
+                n /= 2;
+            }
+            return s;
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.Write(result);
+        }
+    }
+}
