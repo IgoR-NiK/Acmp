0df32531656bb5d56bcea6e138f2e0e76693d6d6
Igor Nikolaev <Igor_2501@mail.ru>
2018-06-29 02:24:52 +0300

    Решена задача 14 - "НОК" (Время: 1 сек. Память: 16 Мб Сложность: 24%)

---
+++ CSharp/TasksApp.Test/Tests/Task0014Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0014Test
+    {
+        private void CheckTest(int a, int b, int output)
+        {
+            var test = Task0014.Solve(a, b);
+            Assert.AreEqual(test, output);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(36, 27, 108);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(39, 65, 195);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0014.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 14 - "НОК"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 24%)
+    /// 
+    /// Требуется написать программу, определяющую наименьшее общее кратное (НОК) чисел a и b.
+    /// 
+    /// Входные данные
+    /// В единственной строке входного файла INPUT.TXT записаны два натуральных числа А и В через пробел, не превышающих 46340.
+    /// 
+    /// Выходные данные
+    /// В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — НОК чисел А и В.
+    /// </summary>
+    public class Task0014
+    {
+        public static void Main()
+        {
+            int a, b;
+            GetInputData(out a, out b);
+
+            var result = Solve(a, b);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int a, out int b)
+        {
+            var input = Console.ReadLine().Split();
+
+            a = int.Parse(input[0]);
+            b = int.Parse(input[1]);
+        }
+
+        public static int Solve(int a, int b)
+        {
+            var c = a * b;
+            while (a * b > 0)
+            {
+                if (a >= b)
+                    a = a % b;
+                else
+                    b = b % a;
+            }
+            var nod = a + b;
+            
+            return c / nod;
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.Write(result);
+        }
+    }
+}
