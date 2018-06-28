4ee68062be73a4bfa1f47db5a89e03a63b5d36fc
Igor Nikolaev <Igor_2501@mail.ru>
2018-06-29 02:35:32 +0300

    Решена задача 21 - "Зарплата" (Время: 1 сек. Память: 16 Мб Сложность: 4%)

---
+++ CSharp/TasksApp.Test/Tests/Task0021Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0021Test
+    {
+        private void CheckTest(int a, int b, int c, int output)
+        {
+            var test = Task0021.Solve(a, b, c);
+            Assert.AreEqual(test, output);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(100, 500, 1000, 900);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(36, 11, 20, 25);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0021.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 21 - "Зарплата"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 4%)
+    /// 
+    /// В отделе работают 3 сотрудника, которые получают заработную плату в рублях. 
+    /// Требуется определить: на сколько зарплата самого высокооплачиваемого из них отличается от самого низкооплачиваемого.
+    /// 
+    /// Входные данные
+    /// В единственной строке входного файла INPUT.TXT записаны размеры зарплат всех сотрудников через пробел. 
+    /// Каждая заработная плата – это натуральное число, не превышающее 10^5.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT необходимо вывести одно целое число — разницу между максимальной и минимальной зарплатой.
+    /// </summary>
+    public class Task0021
+    {
+        public static void Main()
+        {
+            int a, b, c;
+            GetInputData(out a, out b, out c);
+
+            var result = Solve(a, b, c);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int a, out int b, out int c)
+        {
+            var input = Console.ReadLine().Split();
+
+            a = int.Parse(input[0]);
+            b = int.Parse(input[1]);
+            c = int.Parse(input[2]);
+        }
+
+        public static int Solve(int a, int b, int c)
+        {
+            return Math.Max(a, Math.Max(b, c)) - Math.Min(a, Math.Min(b, c));
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.Write(result);
+        }
+    }
+}
