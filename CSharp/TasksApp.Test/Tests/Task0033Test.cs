cb869c0ebb77eb716cb198af41f169ada9f21a75
Igor Nikolaev <Igor_2501@mail.ru>
2018-06-29 15:12:59 +0300

    Решена задача 33 - "Два бандита" (Время: 1 сек. Память: 16 Мб Сложность: 4%

---
+++ CSharp/TasksApp.Test/Tests/Task0033Test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0033Test
+    {
+		private void CheckTest(int a, int b, Task0033.Result result)
+        {
+            var test = Task0033.Solve(a, b);
+            Assert.AreEqual(test.A, result.A);
+			Assert.AreEqual(test.B, result.B);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+			CheckTest(4, 7, new Task0033.Result() { A = 6, B = 3 });
+        }
+    }
+}
