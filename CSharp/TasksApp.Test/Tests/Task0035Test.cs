12af08051d849134390d1b0944f82a5fd99ef7ce
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-01 11:15:04 +0300

    Решена задача 35 - "Конечные автоматы" (Время: 1 сек. Память: 16 Мб Сложность: 11%)

---
+++ CSharp/TasksApp.Test/Tests/Task0035Test.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0035Test
+    {
+        private void CheckTest(int[] n, int[] m, long[] result)
+        {
+            var test = Task0035.Solve(n, m);
+            for (var i = 0; i < n.Length; i++)
+            {
+                Assert.AreEqual(test[i], result[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(new[] { 2, 13, 5, 18 }, new[] { 0, 20, 23, 6 }, new long[] { 44344, 48134, 45699, 49458 });
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(new[] { 15, 1000 }, new[] { 20, 26000 }, new long[] { 48767, 1340237 });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0035.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 35 - "Конечные автоматы"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 11%)
+    /// 
+    /// Однажды известный профессор обнаружил описания k конечных автоматов. 
+    /// По его мнению, нетривиальность конечного автомата, имеющего n состояний и m переходов, можно описать целым числом 
+    /// d = 19m + (n + 239)*(n + 366) / 2 . Чем больше d, тем больший интерес для науки представляет изучение его свойств.
+    /// Помогите профессору вычислить нетривиальность имеющихся у него автоматов.
+    /// 
+    /// Входные данные
+    /// Первая строка входного файла INPUT.TXT содержит целое число k (1 ≤ k ≤ 10000) – количество конечных автоматов. 
+    /// Следующие k строк содержат по два целых числа n(i) (0 ≤ n(i) ≤ 1000) и m(i) (0 ≤ m(i) ≤ 26*n(i)^2) – число состояний и переходов i-го автомата.
+    /// 
+    /// Выходные данные
+    /// Выходной файл OUTPUT.TXT должен состоять из k строк. На i-й строке выходного файла выведите одно число – нетривиальность i-го автомата.
+    /// </summary>
+    public class Task0035
+    {
+        public static void Main()
+        {
+            int[] n, m;
+            GetInputData(out n, out m);
+
+            var result = Solve(n, m);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int[] n, out int[] m)
+        {
+            var k = int.Parse(Console.ReadLine());
+
+            n = new int[k];
+            m = new int[k];
+            for (var i = 0; i < k; i++)
+            {
+                var input = Console.ReadLine().Split();
+
+                n[i] = int.Parse(input[0]);
+                m[i] = int.Parse(input[1]);
+            }
+        }
+
+        public static long[] Solve(int[] n, int[] m)
+        {
+            var result = new long[n.Length];
+
+            for (var i = 0; i < n.Length; i++)
+            {
+                result[i] = 19 * m[i] + (n[i] + 239) * (n[i] + 366) / 2;
+            }
+            return result;
+        }
+
+        private static void PrintResult(long[] result)
+        {
+            Array.ForEach(result, x => Console.WriteLine(x));
+        }
+    }
+}
