6f40eb28784edb0ae41add7bb7278e434773e8d9
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-05 01:11:55 +0300

    Решена задача 52 - "Счастливый билет" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task0052Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0052Test
+    {
+        private void CheckTest(int n, string result)
+        {
+            var test = Task0052.Solve(n);
+			Assert.AreEqual(test, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+			CheckTest(385916, "YES");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(123456, "NO");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            CheckTest(9450, "YES");
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            CheckTest(1, "NO");
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0052.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+	/// Задача 52 - "Счастливый билет"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 12%)
+    /// 
+	/// Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером. 
+    /// Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех. 
+    /// Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
+    /// 
+    /// Входные данные
+	/// В единственной строке входного файла INPUT.TXT записано одно целое число N (0 ≤ N < 10^6).
+    /// 
+    /// Выходные данные
+	/// В выходной файл OUTPUT.TXT нужно вывести «YES», если билет с номером N счастливый и «NO» в противном случае.
+    /// </summary>
+	public class Task0052
+	{
+		public static void Main()
+		{
+			int n;
+			GetInputData(out n);
+
+			var result = Solve(n);
+
+			PrintResult(result);
+		}
+
+		private static void GetInputData(out int n)
+		{
+            n = int.Parse(Console.ReadLine());
+		}
+
+		public static string Solve(int n)
+		{
+            var str = n.ToString().PadLeft(6, '0');
+            return str[0] + str[1] + str[2] == str[3] + str[4] + str[5] ? "YES" : "NO";
+        }
+
+		private static void PrintResult(string result)
+		{
+			Console.WriteLine(result);
+		}
+	}
+}
