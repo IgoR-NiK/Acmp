932f69d233e32ed5e593bdadcbf15dcbfbe60d47
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-04 21:27:31 +0300

    Решена задача 43 - "Нули" (Время: 1 сек. Память: 16 Мб Сложность: 16%)

---
+++ CSharp/TasksApp.Test/Tests/Task0043Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0043Test
+    {
+        private void CheckTest(string input, int result)
+        {
+            var test = Task0043.Solve(input);
+			Assert.AreEqual(test, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+			CheckTest("00101110000110", 4);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest("00", 2);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            CheckTest("111", 0);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0043.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+	/// Задача 43 - "Нули"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 16%)
+    /// 
+	/// Требуется найти самую длинную непрерывную цепочку нулей в последовательности нулей и единиц.
+    /// 
+    /// Входные данные
+	/// В единственной строке входного файла INPUT.TXT записана последовательность нулей и единиц (без пробелов). Суммарное количество цифр от 1 до 100.
+    /// 
+    /// Выходные данные
+	/// В единственную строку выходного файла OUTPUT.TXT нужно вывести искомую длину цепочки нулей.
+    /// </summary>
+	public class Task0043
+	{
+		public static void Main()
+		{
+			string input;
+			GetInputData(out input);
+
+			var result = Solve(input);
+
+			PrintResult(result);
+		}
+
+		private static void GetInputData(out string input)
+		{
+            input = Console.ReadLine();
+		}
+
+		public static int Solve(string input)
+		{
+            int max = 0;
+            for (int i = 0, current = 0; i < input.Length; i++)
+            {
+                current = input[i] == '0' ? current + 1 : 0;
+                max = Math.Max(max, current);
+            }
+
+            return max;
+		}
+
+		private static void PrintResult(int result)
+		{
+			Console.WriteLine(result);
+		}
+	}
+}
