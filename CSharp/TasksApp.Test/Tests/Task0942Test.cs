b6c40ad1320718fb8a5de11db7934422ecb946c8
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-05 16:40:01 +0300

    Решена задача 942 - "Олимпиада" (Время: 1 сек. Память: 16 Мб Сложность: 2%)

---
+++ CSharp/TasksApp.Test/Tests/Task0942Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0942Test
+    {
+        private void CheckTest(int[] tasks, int result)
+        {
+            var test = Task0942.Solve(tasks);
+			Assert.AreEqual(test, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+			CheckTest(new[] { 40, 30, 60 }, 1);
+        }
+
+		[TestMethod]
+		public void Test2()
+		{
+			CheckTest(new[] { 10, 20, 30, 40 }, 1);
+		}
+    }
+}
