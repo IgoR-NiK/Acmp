6e071901006bc3deca7d991f719e5d99bfe33f2d
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-05 16:25:08 +0300

    Решена задача 970 - "От перестановки что-то меняется ..." (Время: 1 сек. Память: 16 Мб Сложность: 9%)

---
+++ CSharp/TasksApp.Test/Tests/Task0970Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0970Test
+    {
+        private void CheckTest(int a1, int a2, int a3, string result)
+        {
+            var test = Task0970.Solve(a1, a2, a3);
+			Assert.AreEqual(test, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(3, 5, 2, "YES");
+        }
+
+		[TestMethod]
+		public void Test2()
+		{
+			CheckTest(2, 2, 5, "NO");
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			CheckTest(2, 2, 4, "YES");
+		}
+    }
+}
