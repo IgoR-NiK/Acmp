7b74a0e134710afe9da9dc0d6c162415b97d6ae5
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-08 22:54:37 +0300

    Решена задача 62 - "Клетки" (Время: 1 сек. Память: 16 Мб Сложность: 15%)

---
+++ CSharp/TasksApp.Test/Tests/Task0062Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0062Test
+    {
+        private void CheckTest(string input, string result)
+        {
+            var resultTest = Task0062.Solve(input);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest("C3", "BLACK");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest("G8", "WHITE");
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0062.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 62 - "Клетки"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 15%)
+    /// 
+    /// Известно, что шахматная доска имеет размерность 8х8 и состоит из клеток 2х цветов, например, черного и белого (см. рисунок). 
+    /// Каждая клетка имеет координату, состоящую из буквы и цифры. Горизонтальное расположение клетки определяется буквой от A до H, 
+    /// а вертикальное – цифрой от 1 до 8. Заметим, что клетка с координатой А1 имеет черный цвет. Требуется по заданной координате определить цвет клетки.
+    /// 
+    /// Входные данные
+    /// В единственной строке входного файла INPUT.TXT записана координата клетки на шахматной доске: всего два символа – буква и цифра (без пробелов).
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT нужно вывести «WHITE», если указанная клетка имеет белый цвет и «BLACK», если она черная.
+    /// </summary>
+    public static class Task0062 
+    {
+        public static void Main()
+        {
+            string input;
+            GetInputData(out input);
+
+            var result = Solve(input);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out string input)
+        {
+            input = Console.ReadLine();          
+        }
+
+        public static string Solve(string input)
+        {
+            int X = input[0] - 'A' + 1;
+            int Y = Convert.ToInt32(input[1]);
+            
+            return (X + Y) % 2 == 0 ? "BLACK" : "WHITE";
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
