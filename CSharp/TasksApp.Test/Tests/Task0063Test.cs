6a8c5647bc65007339ba2302ae618c1c03b1fab6
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-08 23:08:19 +0300

    Решена задача 63 - "Загадка" (Время: 1 сек. Память: 16 Мб Сложность: 18%)

---
+++ CSharp/TasksApp.Test/Tests/Task0063Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0063Test
+    {
+        private void CheckTest(int S, int P, Task0063.Result result)
+        {
+            var resultTest = Task0063.Solve(S, P);
+            Assert.AreEqual(resultTest.X, result.X);
+            Assert.AreEqual(resultTest.Y, result.Y);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(4, 4, new Task0063.Result() { X = 2, Y = 2 });
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(5, 6, new Task0063.Result() { X = 2, Y = 3 });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0063.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 63 - "Загадка"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 18%)
+    /// 
+    /// Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. 
+    /// Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. Для этого Петя делает две подсказки. 
+    /// Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит два натуральных числа S и P, разделенных пробелом.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите два числа Х и Y, загаданные Петей. 
+    /// Числа следует вывести в порядке неубывания своих значений, разделенные пробелом.
+    /// </summary>
+    public static class Task0063 
+    {
+        public static void Main()
+        {
+            int s, p;
+            GetInputData(out s, out p);
+
+            var result = Solve(s, p);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int s, out int p)
+        {
+            var input = Console.ReadLine().Split();
+            s = int.Parse(input[0]);
+            p = int.Parse(input[1]);
+        }
+
+        public static Result Solve(int s, int p)
+        {
+            for (var x = 1; x <= 1000; x++)
+            {
+                for (var y = 1; y <= 1000; y++)
+                {
+                    if (x * y == p && x + y == s)
+                    {
+                        return new Result() { X = x > y ? y : x, Y = x > y ? x : y };
+                    }
+                }
+            }
+
+            return new Result();
+        }
+
+        private static void PrintResult(Result result)
+        {
+            Console.WriteLine(result);
+        }
+
+        public struct Result
+        {
+            public int X;
+            public int Y;
+
+            public override string ToString()
+            {
+                return String.Format("{0} {1}", X > Y ? Y : X, X > Y ? X : Y);
+            }
+        }
+    }
+}
