db4ee73cb1f618913be18ea93aa3f3320ad6ba10
Igor Nikolaev <Igor_2501@mail.ru>
2018-07-11 21:46:42 +0300

    Решена задача 102 - "Треугольник и точка" (Время: 1 сек. Память: 16 Мб Сложность: 32%)

---
+++ CSharp/TasksApp.Test/Tests/Task0102Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0102Test
+    {
+        private void CheckTest(Task0102.Point[] points, string result)
+        {
+            var resultTest = Task0102.Solve(points);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(new[]{
+                new Task0102.Point() { X = 0, Y = 0 },
+                new Task0102.Point() { X = 100, Y = 0 },
+                new Task0102.Point() { X = 0, Y = 100 },
+                new Task0102.Point() { X = 100, Y = 100 } }, "Out");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(new[]{
+                new Task0102.Point() { X = 0, Y = 0 },
+                new Task0102.Point() { X = 100, Y = 0 },
+                new Task0102.Point() { X = 0, Y = 100 },
+                new Task0102.Point() { X = 10, Y = 10 } }, "In");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            CheckTest(new[]{
+                new Task0102.Point() { X = 0, Y = 0 },
+                new Task0102.Point() { X = 100, Y = 0 },
+                new Task0102.Point() { X = 0, Y = 100 },
+                new Task0102.Point() { X = 50, Y = 50 } }, "In");
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            CheckTest(new[]{
+                new Task0102.Point() { X = 0, Y = 0 },
+                new Task0102.Point() { X = 100, Y = 0 },
+                new Task0102.Point() { X = 0, Y = 100 },
+                new Task0102.Point() { X = 0, Y = 0 } }, "In");
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0102.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 102 - "Треугольник и точка"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 32%)
+    /// 
+    /// В декартовой системе координат на плоскости заданы координаты вершин треугольника и еще одной точки. 
+    /// Требуется написать программу, определяющую, принадлежит ли эта точка треугольнику.
+    /// 
+    /// Входные данные
+    /// В четырех строках входного файла INPUT.TXT находятся пары целых чисел - координаты точек. 
+    /// Числа в первых трех строках - это координаты вершин треугольника (x1,y1), (x2,y2), (х3,у3), 
+    /// в четвертой строке - координаты тестируемой точки (x4,у4). Все координаты не превышают 10000 по абсолютной величине.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT необходимо вывести слово «In», если точка находится внутри треугольника и «Out» в противном случае.
+    /// </summary>
+    public static class Task0102
+    {
+        public static void Main()
+        {
+            Point[] points;
+            GetInputData(out points);
+
+            var result = Solve(points);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out Point[] points)
+        {
+            const int countPoints = 4;
+            points = new Point[countPoints];
+            for (var i = 0; i < countPoints; i++)
+            {
+                var input = Console.ReadLine().Split();
+                points[i] = new Point() { X = int.Parse(input[0]), Y = int.Parse(input[1]) };
+            }
+        }
+
+        public static string Solve(Point[] points)
+        {
+            var s1 = GetArea(points[0], points[1], points[2]);
+            var s2 = GetArea(points[0], points[1], points[3]);
+            var s3 = GetArea(points[0], points[2], points[3]);
+            var s4 = GetArea(points[1], points[2], points[3]);
+
+            return s1 == s2 + s3 + s4 ? "In" : "Out";
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+
+        private static double GetArea(Point A, Point B, Point C)
+        {
+            var a = Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+            var b = Math.Sqrt(Math.Pow(C.X - B.X, 2) + Math.Pow(C.Y - B.Y, 2));
+            var c = Math.Sqrt(Math.Pow(C.X - A.X, 2) + Math.Pow(C.Y - A.Y, 2));
+            var p = (a + b + c) / 2;
+            return Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 3);
+        }
+
+        public struct Point
+        {
+            public int X;
+            public int Y;
+        }
+    }
+}
