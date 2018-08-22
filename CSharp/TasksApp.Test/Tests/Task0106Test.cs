e3b40e456d438165a24d3b62b099b73a66c6b314
Igor Nikolaev <Igor_2501@mail.ru>
2018-08-22 21:45:19 +0300

    Решена задача 106 - "Монетки" (Время: 1 сек. Память: 16 Мб Сложность: 8%)

---
+++ CSharp/TasksApp.Test/Tests/Task0106Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0106Test
+    {
+        private void CheckTest(int[] array, int result)
+        {
+            var resultTest = Task0106.Solve(array);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(new []{ 1, 0, 1, 1, 0 }, 2);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0106.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 106 - "Монетки"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 8%)
+    /// 
+    /// На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. 
+    /// Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной.
+    /// 
+    /// Входные данные
+    /// В первой строке входного файла INPUT.TXT записано натуральное число N (1 ≤ N ≤ 100) – число монеток. 
+    /// В каждой из последующих N строк содержится одно целое число – 1 если монетка лежит решкой вверх и 0 если вверх гербом.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите минимальное количество монет, которые нужно перевернуть.
+    /// </summary>
+    public class Task0106
+    {
+        public static void Main()
+        {
+            int[] array;
+            GetInputData(out array);
+
+            var result = Solve(array);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int[] array)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            array = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public static int Solve(int[] array)
+        {
+            return Math.Min(array.Count(x => x == 1), array.Count(x => x == 0));
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
