65edf209bb75c47af69139545384b1d164d58a9a
Igor Nikolaev <Igor_2501@mail.ru>
2018-09-10 16:09:17 +0300

    Решена задача 108 - "Неглухой телефон" (Время: 1 сек. Память: 16 Мб Сложность: 1%)

---
+++ CSharp/TasksApp.Test/Tests/Task0108Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0108Test
+    {
+        private void CheckTest(int input, int result)
+        {
+            var resultTest = Task0108.Solve(input);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(5, 5);
+        }
+    }
+}
