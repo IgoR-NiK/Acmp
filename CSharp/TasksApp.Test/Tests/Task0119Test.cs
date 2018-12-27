aea7751ae784116a8dac8b35df2fe7d9b947bd8f
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-27 10:35:22 +0300

    Решена задача 119 - "Сортировка времени" (Время: 1 сек. Память: 16 Мб Сложность: 23%)

---
+++ CSharp/TasksApp.Test/Tests/Task0119Test.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0119Test
+    {
+        private void CheckTest(Task0119.TimeInterval[] input, Task0119.TimeInterval[] result)
+        {
+            var resultTest = Task0119.Solve(input);
+
+			Assert.AreEqual(resultTest.Length, result.Length);
+
+			var comparer = new Task0119.TimeIntervalComparer();
+			for (var i = 0; i < result.Length; i++)
+			{
+				Assert.AreEqual(comparer.Compare(resultTest[i], result[i]), 0);
+			}
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(new[] 
+			{
+				new Task0119.TimeInterval() { Hours = 10, Minutes = 20, Seconds = 30 },
+				new Task0119.TimeInterval() { Hours = 7, Minutes = 30, Seconds = 0 },
+				new Task0119.TimeInterval() { Hours = 23, Minutes = 59, Seconds = 59 },
+				new Task0119.TimeInterval() { Hours = 13, Minutes = 30, Seconds = 30 }
+			}, new[]
+			{
+				new Task0119.TimeInterval() { Hours = 7, Minutes = 30, Seconds = 0 },
+				new Task0119.TimeInterval() { Hours = 10, Minutes = 20, Seconds = 30 },
+				new Task0119.TimeInterval() { Hours = 13, Minutes = 30, Seconds = 30 },
+				new Task0119.TimeInterval() { Hours = 23, Minutes = 59, Seconds = 59 },
+			});
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0119.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 119 - "Сортировка времени"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 23%)
+	/// 
+	/// Требуется выполнить сортировку временных моментов, заданных в часах, минутах и секундах.
+	/// 
+	/// Входные данные
+	/// Во входном файле INPUT.TXT в первой строке записано число N (1 ≤ N ≤ 100), а в последующих N строках N моментов времени. 
+	/// Каждый момент времени задается 3 целыми числами - часы (от 0 до 23), минуты (от 0 до 59) и секунды (от 0 до 59).
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите моменты времени, упорядоченные в порядке неубывания без ведущих нулей.
+	/// </summary>
+	public class Task0119
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+
+            var result = Solve(input);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out TimeInterval[] input)
+        {
+			var n = int.Parse(Console.ReadLine());
+
+			input = new TimeInterval[n];
+			for (var i = 0; i < n; i++)
+			{
+				var time = Console.ReadLine().Split();
+				input[i] = new TimeInterval()
+				{
+					Hours = int.Parse(time[0]),
+					Minutes = int.Parse(time[1]),
+					Seconds = int.Parse(time[2])
+				};
+			}
+        }
+
+        public static TimeInterval[] Solve(TimeInterval[] input)
+        {
+			Array.Sort(input, new TimeIntervalComparer());
+			return input;
+        }
+
+        private static void PrintResult(TimeInterval[] result)
+        {
+            foreach (var time in result)
+			{
+				Console.WriteLine($"{time.Hours} {time.Minutes} {time.Seconds}");
+			}
+        }
+
+		public class TimeInterval
+		{
+			public int Hours { get; set; }
+			public int Minutes { get; set; }
+			public int Seconds { get; set; }
+		}
+
+		public class TimeIntervalComparer : IComparer<TimeInterval>
+		{
+			public int Compare(TimeInterval x, TimeInterval y)
+			{
+				if (x.Hours > y.Hours) return 1;
+				if (x.Hours < y.Hours) return -1;
+
+				if (x.Minutes > y.Minutes) return 1;
+				if (x.Minutes < y.Minutes) return -1;
+
+				if (x.Seconds > y.Seconds) return 1;
+				if (x.Seconds < y.Seconds) return -1;
+
+				return 0;
+			}
+		}
+	}
+}
