a7b8d21003f75186680694d43cc40823fbc9a702
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-28 10:06:33 +0300

    Решена задача 149 - "Разворот" (Время: 1 сек. Память: 16 Мб Сложность: 9%)

---
+++ CSharp/TasksApp.Test/Tests/Task0149Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+	[TestClass]
+	public class Task0149Test
+	{
+		private void CheckTest(string[] input, string[] result)
+		{
+			var resultTest = Task0149.Solve(input);
+			for (var i = 0; i < result.Length; i++)
+			{
+				Assert.AreEqual(result[i], resultTest[i]);
+			}
+			
+		}
+
+		[TestMethod]
+		public void Test1()
+		{
+			CheckTest(new[] { "1", "2", "3" }, new[] { "3", "2", "1" });
+		}		
+	}
+}
---
+++ CSharp/TasksApp/Tasks/Task0149.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 149 - "Разворот"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 9%)
+	/// 
+	/// Дано натуральное число N и последовательность из N элементов. Требуется вывести эту последовательность в обратном порядке.
+	/// 
+	/// Входные данные
+	/// В первой строке входного файла INPUT.TXT записано натуральное число N (N ≤ 10^3). Во второй строке через пробел идут N целых чисел, по модулю не превосходящих 10^3 - элементы последовательности.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите заданную последовательность в обратном порядке.
+	/// </summary>
+	public class Task0149
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+						
+			var result = Solve(input);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out string[] input)
+        {		
+			var n = int.Parse(Console.ReadLine());
+			input = Console.ReadLine().Split();
+        }
+
+        public static string[] Solve(string[] input)
+        {
+			Array.Reverse(input);
+			return input;
+        }
+
+        private static void PrintResult(string[] result)
+        {
+			Console.Write(String.Join(" ", result));
+        }
+	}
+}
