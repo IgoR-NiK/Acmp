f5619058a2f3e4db843e9bada99a6296ce5ea526
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-28 09:55:47 +0300

    Решена задача 147 - "Числа Фибоначчи" (Время: 1 сек. Память: 16 Мб Сложность: 16%)

---
+++ CSharp/TasksApp.Test/Tests/Task0147Test.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+	[TestClass]
+	public class Task0147Test
+	{
+		private void CheckTest(int n, int result)
+		{
+			var resultTest = Task0147.Solve(n);
+			Assert.AreEqual(resultTest, result);
+		}
+
+		[TestMethod]
+		public void Test1()
+		{
+			CheckTest(7, 13);
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			CheckTest(0, 0);
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			CheckTest(1, 1);
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			CheckTest(2, 1);
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			CheckTest(11, 89);
+		}
+
+		[TestMethod]
+		public void Test6()
+		{
+			CheckTest(30, 832040);
+		}
+	}
+}
---
+++ CSharp/TasksApp/Tasks/Task0147.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 147 - "Числа Фибоначчи"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 16%)
+	/// 
+	/// Последовательностью Фибоначчи называется последовательность чисел a(0), a(1), ..., a(n), ..., где a(0) = 0, a(1) = 1, a(k) = a(k-1) + a(k-2) (k > 1).
+	/// Требуется найти N-е число Фибоначчи.
+	/// 
+	/// Входные данные
+	/// Во входном файле INPUT.TXT записано целое число N (0 ≤ N ≤ 30).
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите N-е число Фибоначчи.
+	/// </summary>
+	public class Task0147
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+						
+			var result = Solve(n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int n)
+        {			
+			n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+			if (n == 0) return 0;
+			if (n == 1) return 1;
+
+			var oldValue = 0;
+			var value = 1;
+			var temp = 0;
+			for (var i = 2; i < n + 1; i++)
+			{
+				temp = value;
+				value += oldValue;
+				oldValue = temp;
+			}
+
+			return value;
+        }
+
+        private static void PrintResult(int result)
+        {
+			Console.Write(result);
+        }
+	}
+}
