cdf93437cde647c94679c9bb78f04aacf48f97ea
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-29 10:05:47 +0300

    Решена задача 195 - "Эния" (Время: 1 сек. Память: 16 Мб Сложность: 3%)

---
+++ CSharp/TasksApp.Test/Tests/Task0195Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0195Test
+    {
+        private void CheckTest(int n, int a, int b, int result)
+        {
+            var resultTest = Task0195.Solve(n, a, b);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(5, 2, 3, 60);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(14, 23, 5, 3220);
+        }
+    }
+}
