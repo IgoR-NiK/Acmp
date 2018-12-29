298d05829ad42a12b77983b71bb575b46b2ca035
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-29 10:17:51 +0300

    Решена задача 773 - "Гулливер" (Время: 1 сек. Память: 16 Мб Сложность: 4%)

---
+++ CSharp/TasksApp.Test/Tests/Task0773Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0773Test
+    {
+        private void CheckTest(int k, int m, int result)
+        {
+            var resultTest = Task0773.Solve(k, m);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(2, 2, 8);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(12, 4, 576);
+        }
+    }
+}
