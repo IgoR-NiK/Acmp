56ca4e5343c743c44a507e5c8b9d5e76d8b690cd
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-29 10:40:57 +0300

    Решена задача 539 - "Торт" (Время: 1 сек. Память: 16 Мб Сложность: 6%)

---
+++ CSharp/TasksApp.Test/Tests/Task0539Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0539Test
+    {
+        private void CheckTest(int n, int result)
+        {
+            var resultTest = Task0539.Solve(n);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(2, 1);
+        }
+
+		[TestMethod]
+		public void Test2()
+		{
+			CheckTest(1, 0);
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			CheckTest(3, 3);
+		}
+	}
+}
