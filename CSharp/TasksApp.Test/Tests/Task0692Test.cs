199072726eb93ef742b629daeb58fd30cb5e9cf0
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-29 11:16:18 +0300

    Решена задача 692 - "Бинарные числа" (Время: 1 сек. Память: 16 Мб Сложность: 8%)

---
+++ CSharp/TasksApp.Test/Tests/Task0692Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0692Test
+    {
+        private void CheckTest(int n, string result)
+        {
+            var resultTest = Task0692.Solve(n);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(1024, "YES");
+        }
+
+		[TestMethod]
+		public void Test2()
+		{
+			CheckTest(1000, "NO");
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			CheckTest(23, "NO");
+		}
+	}
+}
