c62350378c2b64bd98dada33e2fbf26d714ffc97
Igor Nikolaev <Igor_2501@mail.ru>
2018-12-29 10:28:08 +0300

    Решена задача 755 - "Сбор земляники" (Время: 1 сек. Память: 16 Мб Сложность: 6%)

---
+++ CSharp/TasksApp.Test/Tests/Task0755Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0755Test
+    {
+        private void CheckTest(int x, int y, int z, string result)
+        {
+            var resultTest = Task0755.Solve(x, y, z);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(3, 2, 1, "4");
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+			CheckTest(12, 13, 5, "20");
+		}
+
+		[TestMethod]
+		public void Test3()
+		{
+			CheckTest(2, 5, 9, "Impossible");
+		}
+	}
+}
---
+++ CSharp/TasksApp/Tasks/Task0755.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 755 - "Сбор земляники"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 6%)
+	/// 
+	/// Маша и Миша собирали землянику. Маше удалось сорвать X ягод, а Мише – Y ягод. Поскольку ягода была очень вкусной, 
+	/// то ребята могли какую то часть ягод съесть. По нашим подсчетам вместе они съели Z ягод.
+	///
+	/// Требуется определить: сколько ягод ребята собрали в результате, при этом следует проверить, не ошиблись ли мы в расчетах, 
+	/// подсчитывая количество съеденных ягод (их не должно было получиться больше, чем сорванных ягод).
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит три натуральных числа X, Y и Z, не превышающих 1000. Все числа расположены в первой строке файла и разделены пробелом.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите количество собранных ягод, если наши подсчеты оказались правдоподобными, либо слово «Impossible» в противном случае.
+	/// </summary>
+	public static class Task0755
+    {
+        public static void Main()
+        {            
+            GetInputData(out var x, out var y, out var z);
+
+            var result = Solve(x, y, z);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int x, out int y, out int z)
+        {
+            var inputData = Console.ReadLine().Split();
+            x = int.Parse(inputData[0]);
+            y = int.Parse(inputData[1]);
+			z = int.Parse(inputData[2]);
+		}
+
+        public static string Solve(int x, int y, int z)
+        {
+            return x + y < z ? "Impossible" : $"{x + y - z}";
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
