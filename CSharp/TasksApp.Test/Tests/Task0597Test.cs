327173b514d04b343300d792b3d572b691c3c3ca
Igor Nikolaev <Igor_2501@mail.ru>
2019-01-01 15:38:26 +0300

    Решена задача 597 - "Внеземные гости" (Время: 1 сек. Память: 16 Мб Сложность: 10%)

---
+++ CSharp/TasksApp.Test/Tests/Task0597Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0597Test
+    {
+        private void CheckTest(int r1, int r2, int r3, string result)
+        {
+            var resultTest = Task0597.Solve(r1, r2, r3);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(10, 10, 10, "NO");
+        }
+
+		[TestMethod]
+		public void Test2()
+		{
+			CheckTest(10, 3, 4, "YES");
+		}
+	}
+}
