6ede5056e85df14f6f8f2bd93d065e7881a1dfcb
Igor Nikolaev <Igor_2501@mail.ru>
2019-01-09 23:56:59 +0300

    Решена задача 685 - "Золотой песок" (Время: 1 сек. Память: 16 Мб Сложность: 10%)

---
+++ CSharp/TasksApp.Test/Tests/Task0685Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0685Test
+    {
+        private void CheckTest(int[] a, int[] b, int result)
+        {
+            var resultTest = Task0685.Solve(a, b);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(new[] { 1, 2, 3 }, new[] { 3, 2, 1 }, 14);
+        }		
+	}
+}
