690a70876e73db3cf8e8fe07e189083ae3cf7de3
Igor Nikolaev <Igor_2501@mail.ru>
2020-01-11 21:04:05 +0300

    Решена задача 196 - "Спираль" (Время: 1 сек. Память: 16 Мб Сложность: 38%)

---
+++ CSharp/TasksApp.Test/Tests/Task0196Test.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestClass]
+    public class Task0196Test
+    {
+        private void CheckTest(int n, int[][] result)
+        {
+            var solution = Task0196.Solve(n);
+
+            Assert.AreEqual(solution.Length, result.Length);
+            for (var i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(solution[i].Length, result[i].Length);
+                for (var j = 0; j < result[i].Length; j++)
+                {
+                    Assert.AreEqual(solution[i][j], result[i][j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            CheckTest(
+                3,
+                new int[][] {
+                    new [] { 1, 2, 3 },
+                    new [] { 8, 9, 4 },
+                    new [] { 7, 6, 5 }
+                });
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            CheckTest(
+                4,
+                new int[][] {
+                    new [] { 1, 2, 3, 4 },
+                    new [] { 12, 13, 14, 5 },
+                    new [] { 11, 16, 15, 6 },
+                    new [] { 10, 9, 8, 7 }
+                });
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            CheckTest(
+                5,
+                new int[][] {
+                    new [] { 1, 2, 3, 4, 5 },
+                    new [] { 16, 17, 18, 19, 6 },
+                    new [] { 15, 24, 25, 20, 7 },
+                    new [] { 14, 23, 22, 21, 8 },
+                    new [] { 13, 12, 11, 10, 9 }
+                });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0196.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 196 - "Спираль"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 38%)
+    /// 
+    /// Требуется вывести квадрат, состоящий из N×N клеток, заполненных числами от 1 до N^2 по спирали (см. примеры).
+    /// 
+    /// Входные данные
+    /// Во входном файле INPUT.TXT задано целое число N – размер квадратной матрицы (2 ≤ N ≤ 100).
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите матрицу, заполненную числами от 1 до N^2 по спирали, 
+    /// при этом между числами может быть любое количество пробелов. Не допускается начинать спираль в ином, 
+    /// кроме верхнего левого, углу, закручивать спираль против часовой стрелки или изнутри наружу.
+    /// </summary>
+    public static class Task0196 
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+			n = int.Parse(Console.ReadLine());
+        }
+
+        public static int[][] Solve(int n)
+        {
+            var result = new int[n][];
+            for (var i = 0; i < n; i++)
+                result[i] = new int[n];
+
+            var direction = Direction.Right;
+            for (int p = 1, i = 0, j = 0; p <= n * n;)
+            {
+                result[i][j] = p;
+                p++;
+
+                switch (direction)
+                {
+                    case Direction.Up:
+                        if (i - 1 >= 0 && result[i - 1][j] == 0)
+                        {
+                            i--;
+                        }
+                        else
+                        {
+                            j++;
+                            direction = Direction.Right;
+                        }
+                        break;
+                    case Direction.Right:
+                        if (j + 1 < n && result[i][j + 1] == 0)
+                        {
+                            j++;
+                        }
+                        else
+                        {
+                            i++;
+                            direction = Direction.Down;
+                        }
+                        break;
+                    case Direction.Down:
+                        if (i + 1 < n && result[i + 1][j] == 0)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            j--;
+                            direction = Direction.Left;
+                        }
+                        break;
+                    case Direction.Left:
+                        if (j - 1 >= 0 && result[i][j - 1] == 0)
+                        {
+                            j--;
+                        }
+                        else
+                        {
+                            i--;
+                            direction = Direction.Up;
+                        }
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private static void PrintResult(int[][] result)
+        {
+            foreach (var rows in result)
+            {
+                foreach (var column in rows)
+                {
+                    Console.WriteLine($"{column} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        enum Direction
+        {
+            Up,
+            Right,
+            Down,
+            Left
+        }
+    }
+}
