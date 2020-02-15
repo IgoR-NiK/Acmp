8909b1b61dc5ec44d37dfe667823d25fcdb2950f
Igor Nikolaev <Igor_2501@mail.ru>
2020-02-16 00:39:02 +0300

    Решена задача 706 - "Пушка" (Время: 1 сек. Память: 16 Мб Сложность: 20%)

---
+++ CSharp/TasksApp.Test/Tests/Task0706Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0706Test
+    {
+        private static void CheckTest(double r, double x, double y, double result)
+        {
+            var solution = Task0706.Solve(r, x, y);
+			Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+			CheckTest(10, 5, 5, 3.33);
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(10, 10, 5, 6.67);
+		}
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0706.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+	/// Задача 706 - "Пушка"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 20%)
+    /// 
+    /// В начале координат установлена пушка, стреляющая шариками для пинг-понга. На некотором расстоянии R от нее,
+    /// параллельно оси ОХ, находится кирпичная стена бесконечной длины. Между стеной и осью OX расположена точечная цель
+    /// с координатами (X,Y). Требуется нацелить пушку так, чтобы шарик ударился сначала о стену, а затем попал в цель.
+    /// Определите кратчайшее расстояние от оси OY до точки соударения шарика со стеной.
+    ///  
+    /// Входные данные
+	/// Во входном файле INPUT.TXT содержится три целых числа R, X и Y (-10 ≤ X ≤ 10, 0 ≤ Y < R ≤ 10), разделенных пробелами.
+    /// 
+    /// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите расстояние по прямой линии от оси OY до точки удара шарика о стену с точностью не худшей,
+	/// чем два знака после запятой.
+    /// </summary>
+	public class Task0706
+	{
+		public static void Main()
+		{
+			GetInputData(out var r, out var x, out var y);
+
+			var result = Solve(r, x, y);
+
+			PrintResult(result);
+		}
+
+		private static void GetInputData(out double r, out double x, out double y)
+		{
+			var input = Console.ReadLine().Split();
+
+			r = double.Parse(input[0]);
+			x = double.Parse(input[1]);
+			y = double.Parse(input[2]);
+		}
+
+		public static double Solve(double r, double x, double y)
+		{
+			return Math.Abs(Math.Round(x * r / (2 * r - y), 2));
+		}
+
+		private static void PrintResult(double result)
+		{
+			Console.WriteLine($"{result}".Replace(',', '.'));
+		}
+	}
+}
