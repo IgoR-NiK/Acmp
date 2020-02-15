618416cbfc8ca4f345f1bcb289ebf7ff02e7db97
Igor Nikolaev <Igor_2501@mail.ru>
2020-02-16 00:27:49 +0300

    Решена задача 763 - "Игра с ладьей" (Время: 1 сек. Память: 16 Мб Сложность: 19%)

---
+++ CSharp/TasksApp.Test/Tests/Task0763Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0763Test
+    {
+        private void CheckTest(int x, int y, int result)
+        {
+            var solution = Task0763.Solve(x, y);
+			Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+			CheckTest(1, 1, 0);
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(1, 6, 1);
+		}
+    }
+}
