abd4dde4ef9bd77f825a4cb609019ce2f0b01078
Igor Nikolaev <Igor_2501@mail.ru>
2020-02-16 00:03:53 +0300

    Решена задача 233 - "Автобусная экскурсия" (Время: 1 сек. Память: 16 Мб Сложность: 14%)

---
+++ CSharp/TasksApp.Test/Tests/Task0233Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0233Test
+    {
+        private void CheckTest(int n, int[] heights, string result)
+        {
+            var solution = Task0233.Solve(n, heights);
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(1, new[] { 763 }, "No crash");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(3, new[] { 763, 245, 113 }, "Crash 2");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            CheckTest(1, new[] { 437 }, "Crash 1");
+        }
+    }
+}
