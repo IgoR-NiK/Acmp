c070158d6a239303e0b65c178d6deea67985b7a7
Igor Nikolaev <Igor_2501@mail.ru>
2020-02-15 23:37:05 +0300

    Решена задача 197 - "Змейка" (Время: 1 сек. Память: 16 Мб Сложность: 40%)

---
+++ CSharp/TasksApp.Test/Tests/Task0197Test.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0197Test
+    {
+        private void CheckTest(int n, int[][] result)
+        {
+            var solution = Task0197.Solve(n);
+
+            Assert.AreEqual(solution.Length, result.Length);
+            for (var i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(solution[i].Length, result[i].Length);
+                for (var j = 0; j < result[i].Length; j++)
+                {
+                    Assert.AreEqual(solution[i][j], result[i][j]);
+                }
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(
+                3,
+                new int[][] {
+                    new [] { 1, 3, 4 },
+                    new [] { 2, 5, 8 },
+                    new [] { 6, 7, 9 }
+                });
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(
+                4,
+                new int[][] {
+                    new [] { 1, 3, 4, 10 },
+                    new [] { 2, 5, 9, 11 },
+                    new [] { 6, 8, 12, 15 },
+                    new [] { 7, 13, 14, 16 }
+                });
+        }
+
+        [Test]
+        public void Test3()
+        {
+            CheckTest(
+                5,
+                new int[][] {
+                    new [] { 1, 3, 4, 10, 11 },
+                    new [] { 2, 5, 9, 12, 19 },
+                    new [] { 6, 8, 13, 18, 20 },
+                    new [] { 7, 14, 17, 21, 24 },
+                    new [] { 15, 16, 22, 23, 25 }
+                });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0197.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 197 - "Змейка"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 40%)
+    /// 
+    /// Требуется заполнить змейкой квадратную матрицу так, как показано на рисунке справа:
+    /// заполнение происходит с единицы из левого верхнего угла и заканчивается в правом нижнем числом N^2,
+    /// где N – порядок матрицы.
+    /// 
+    /// Входные данные
+    /// Во входном файле INPUT.TXT задано натуральное число N – размер квадратной матрицы (N ≤ 100).
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите матрицу, заполненную числами от 1 до N^2 змейкой,
+    /// при этом между числами может быть любое количество пробелов.
+    /// </summary>
+    public static class Task0197 
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+			n = int.Parse(Console.ReadLine());
+        }
+
+        public static int[][] Solve(int n)
+        {
+            var result = new int[n][];
+            for (var i = 0; i < n; i++)
+                result[i] = new int[n];
+
+            var direction = Direction.DownLeft;
+            for (int p = 1, i = 0, j = 0; p <= n * n; p++)
+            {
+                result[i][j] = p;
+
+                switch (direction)
+                {
+                    case Direction.DownLeft:
+                        if (i + 1 >= n)
+                        {
+                            j++;
+                            direction = Direction.UpRight;
+                        }
+                        else if (j - 1 < 0)
+                        {
+                            i++;
+                            direction = Direction.UpRight;
+                        }
+                        else
+                        {
+                            j--;
+                            i++;
+                        }
+
+                        break;
+                    case Direction.UpRight:
+                        if (j + 1 >= n)
+                        {
+                            i++;
+                            direction = Direction.DownLeft;
+                        } 
+                        else if (i - 1 < 0)
+                        {
+                            j++;
+                            direction = Direction.DownLeft;
+                        }
+                        else
+                        {
+                            i--;
+                            j++;
+                        }
+                        
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private static void PrintResult(int[][] result)
+        {
+            foreach (var rows in result)
+            {
+                foreach (var column in rows)
+                {
+                    Console.WriteLine($"{column} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        enum Direction
+        {
+            DownLeft,
+            UpRight
+        }
+    }
+}
