5e4864661932ba12818fdcf301f9c57567367075
Igor Nikolaev <Igor_2501@mail.ru>
2020-02-20 23:30:55 +0300

    Решена задача 697 - "Ремонт" (Время: 1 сек. Память: 16 Мб Сложность: 11%)

---
+++ CSharp/TasksApp.Test/Tests/Task0697Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0697Test
+    {
+        private static void CheckTest(int n, int v, int k, int result)
+        {
+            var solution = Task0697.Solve(n, v, k);
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+	        CheckTest(8, 8, 2, 4);
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(1, 1, 3, 1);
+		}
+    }
+}
