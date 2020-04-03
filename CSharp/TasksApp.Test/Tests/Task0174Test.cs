79866477c5b5b36c0e7dcc01399b5ee4a6f897a8
Igor Nikolaev <Igor_2501@mail.ru>
2020-04-03 21:54:31 +0300

    Решена задача 174 - "Свадьба" (Время: 1 сек. Память: 16 Мб Сложность: 32%)

---
+++ CSharp/TasksApp.Test/Tests/Task0174Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0174Test
+    {
+        private void CheckTest(double[] b, double a, string result)
+        {
+            var solution = Task0174.Solve(b, a);
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new double[] { 5, 10 }, 5, "7.500000");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new double[] { 1, 3, 2 }, 0, "2.125000");
+        }
+    }
+}
