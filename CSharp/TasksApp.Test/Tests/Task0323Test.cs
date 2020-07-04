bb1ca4857a762021aa749b7ff582e9a4b58cdcb0
Igor Nikolaev <Igor_2501@mail.ru>
2020-07-05 02:39:01 +0300

    Решена задача 323 - "Гипотеза Гольдбаха" (Время: 1 сек. Память: 16 Мб Сложность: 30%)

---
+++ CSharp/TasksApp.Test/Tests/Task0323Test.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0323Test
+    {
+        private void CheckTest(int n, int result)
+        {
+            var solution = Task0323.Solve(n);
+
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(6, 3);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(992, 73);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0323.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 323 - "Гипотеза Гольдбаха"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 30%)
+    /// 
+    /// Известно, что любое чётное число, большее 2, представимо в виде суммы 2 простых чисел, причём таких разложений может быть несколько.
+    /// Впервые гипотезу о существовании данного разложения сформулировал математик Х. Гольдбах.
+    ///
+    /// Требуется написать программу, производящую согласно утверждению Гольдбаха, разложение заданного чётного числа.
+    /// Из всех пар простых чисел, сумма которых равна заданному числу, требуется найти пару, содержащую наименьшее простое число.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит чётное число N (4 ≤ N ≤ 998).
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT необходимо вывести два простых числа, сумма которых равна числу N. Первым выводится наименьшее число.
+    /// </summary>
+    public static class Task0323
+    {
+        public static void Main()
+        {
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(n, result);
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+            for (var i = 2; i <= n / 2; i++)
+            {
+                if (IsPrime(i) && IsPrime(n - i))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        private static void PrintResult(int n, int p)
+        {
+            Console.WriteLine($"{p} {n - p}");
+        }
+
+        private static bool IsPrime(int n)
+        {
+            for (var i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
