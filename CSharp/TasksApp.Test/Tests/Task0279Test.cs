6ca020a892e52239924383dba229c0e5f1f90fe1
Igor Nikolaev <Igor_2501@mail.ru>
2020-07-05 02:22:01 +0300

    Решена задача 296 - "Лиса Алиса и кот Базилио" (Время: 1 сек. Память: 16 Мб Сложность: 22%)

---
+++ CSharp/TasksApp.Test/Tests/Task0279Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0296Test
+    {
+        private void CheckTest(int n, Task0296.Result result)
+        {
+            var solution = Task0296.Solve(n);
+
+            Assert.AreEqual(solution.Basilio, result.Basilio);
+            Assert.AreEqual(solution.Alisa, result.Alisa);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(8, new Task0296.Result(1, 1));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(11, new Task0296.Result(1, 2));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            CheckTest(15, new Task0296.Result(3, 0));
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0296.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 296 - "Лиса Алиса и кот Базилио"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 22%)
+    /// 
+    /// Лиса Алиса и кот Базилио вырастили денежное дерево. И выросли на нем трехрублевые и пятирублевые золотые монеты.
+    /// Лиса Алиса себе взяла трехрублевые монеты, а коту Базилио отдала пятирублевые монеты. Посетовав на свою скромность, она предложила впредь рассчитываться
+    /// за покупки вместе, деньги давать без сдачи и минимальным числом монет. Известно, что они сделали покупку стоимостью N рублей, при этом они рассчитались без сдачи.
+    ///
+    /// Вам следует написать программу, которая определяет: сколько монет внес кот Базилио, и сколько монет внесла лиса Алиса.
+    /// 
+    /// Входные данные
+    /// Во входном файле INPUT.TXT записано одно натуральное число N – стоимость покупки в рублях (7 &lt; N &lt; 1000).
+    /// 
+    /// Выходные данные
+    /// В выходной OUTPUT.TXT выведите два целых числа через пробел: число монет, которые отдал кот Базилио и число монет, которые отдала лиса Алиса.
+    /// </summary>
+    public static class Task0296
+    {
+        public static void Main()
+        {
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static Result Solve(int n)
+        {
+            switch (n % 5)
+            {
+                case 0: return new Result(n / 5, 0);
+                case 1: return new Result((n - 6) / 5, 2);
+                case 2: return new Result((n - 12) / 5, 4);
+                case 3: return new Result((n - 3) / 5, 1);
+                default: return new Result((n - 9) / 5, 3);
+            }
+        }
+
+        private static void PrintResult(Result result)
+        {
+            Console.WriteLine(result);
+        }
+        
+        public class Result
+        {
+            public int Basilio { get; }
+            
+            public int Alisa { get; }
+
+            public Result(int basilio, int alisa)
+            {
+                Basilio = basilio;
+                Alisa = alisa;
+            }
+
+            public override string ToString()
+            {
+                return $"{Basilio} {Alisa}";
+            }
+        }
+    }
+}
