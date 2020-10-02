dfd9c812df6f17935543f5286ddcc5e2829a1047
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-02 15:50:48 +0300

    Решена задача 354 - "Разложение на простые множители" (Время: 1 сек. Память: 16 Мб Сложность: 27%)

---
+++ CSharp/TasksApp.Test/Tests/Task0354Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0354Test
+    {
+        private void CheckTest(int n, int[] result)
+        {
+            var test = Task0354.Solve(n);
+            
+            Assert.AreEqual(result.Length, test.Count());
+
+            var i = 0;
+            foreach (var t in test)
+            {
+                Assert.AreEqual(t, result[i]);
+                i++;
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(5, new[] { 5 });
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(30, new[] { 2, 3, 5 });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0354.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 354 - "Разложение на простые множители"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 27%)
+    /// 
+    /// Требуется вывести представление целого числа N в виде произведения простых чисел.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит натуральное число N (2 ≤ N ≤ 2^(31)-1).
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите список простых множителей
+    /// числа N в порядке неубывания, разделенных знаком «*».
+    /// </summary>
+    public class Task0354
+    {
+        public static void Main()
+        {
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static IEnumerable<int> Solve(int n)
+        {
+            var i = 2;
+
+            while (i <= Math.Sqrt(n))
+            {
+                if (n % i == 0)
+                {
+                    n /= i;
+                    yield return i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            
+            if (n > 1)
+            {
+                yield return n;
+            }
+        }
+
+        private static void PrintResult(IEnumerable<int> result)
+        {
+            Console.Write(string.Join("*", result));
+        }
+    }
+}
