a3b6907693bcb2ad7aedd7f7c19745da480ebce7
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-02 16:43:55 +0300

    Решена задача 364 - "Совершенные числа" (Время: 1 сек. Память: 16 Мб Сложность: 51%)

---
+++ CSharp/TasksApp.Test/Tests/Task0364Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0364Test
+    {
+        private void CheckTest(long m, long n, long[] result)
+        {
+            var test = Task0364.Solve(m, n);
+            
+            Assert.AreEqual(result.Length, test.Count);
+
+            for (var i = 0; i < result.Length; i++)
+                Assert.AreEqual(test[i], result[i]);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(6, 6, new long[] { 6 });
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(4, 5, new long[] { });
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(5, 30, new long[] { 6, 28});
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            CheckTest(2305843008139952128, 2305843008139952128, new long[] { 2305843008139952128});
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0364.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 364 - "Совершенные числа"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 51%)
+    /// 
+    /// Число называется совершенным, если оно равно сумме всех своих делителей, меньших его самого.
+    /// Требуется найти все совершенные числа от M до N.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит числа M и N, разделенные пробелом. (1 ≤ M ≤ N ≤ 5*10^18)
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите по одному числу в строке в порядке возрастания все совершенные числа,
+    /// находящихся на отрезке [M, N]. В том случае, когда таких чисел нет следует вывести «Absent». 
+    /// </summary>
+    public class Task0364
+    {
+        public static void Main()
+        {
+            GetInputData(out var m, out var n);
+
+            var result = Solve(m, n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out long m, out long n)
+        {
+            var input = Console.ReadLine().Split();
+            
+            m = long.Parse(input[0]);
+            n = long.Parse(input[1]);
+        }
+
+        public static List<long> Solve(long m, long n)
+        {
+            var result = new List<long>();
+            
+            for (var p = 2; p < 32; p++)
+            {
+                if (IsPrime((long)Math.Pow(2, p) - 1))
+                {
+                    var k = (long)Math.Pow(2, p - 1) * ((long)Math.Pow(2, p) - 1);
+
+                    if (k >= m && k <= n)
+                    {
+                        result.Add(k);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static void PrintResult(List<long> result)
+        {
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+
+            if (result.Count == 0)
+                Console.WriteLine("Absent");
+        }
+
+        private static bool IsPrime(long n)
+        {
+            for (var i = 2L; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
