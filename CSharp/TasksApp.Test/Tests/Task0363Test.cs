4d0cbc3e0e31b58202e7713bdadb2ae45db3e0a1
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-02 16:04:20 +0300

    Решена задача 363 - "Длинное произведение" (Время: 1 сек. Память: 16 Мб Сложность: 46%)

---
+++ CSharp/TasksApp.Test/Tests/Task0363Test.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0363Test
+    {
+        private void CheckTest(Task0363.BigInteger m, Task0363.BigInteger n, Task0363.BigInteger result)
+        {			
+            var test = Task0363.Solve(m, n);
+			Assert.AreEqual(test.CompareTo(result), 0);
+        }
+
+        [Test]
+        public void Test1()
+        {
+	        CheckTest(
+		        new Task0363.BigInteger("5"),
+		        new Task0363.BigInteger("7"),
+		        new Task0363.BigInteger("35"));
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(
+				new Task0363.BigInteger("4134937827592"),
+				new Task0363.BigInteger("784"),
+				new Task0363.BigInteger("3241791256832128"));
+		}
+
+		[Test]
+		public void Test3()
+		{
+			CheckTest(
+				new Task0363.BigInteger("9876543210"),
+				new Task0363.BigInteger("1023456789"),
+				new Task0363.BigInteger("10108215200126352690"));
+		}
+    }
+}
