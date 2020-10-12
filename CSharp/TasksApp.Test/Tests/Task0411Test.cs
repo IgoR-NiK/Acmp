c6c9fed7b9d82063a2071b542d2a3baaeca7b611
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-13 01:34:30 +0300

    Решена задача 411 - "Квадратное уравнение" (Время: 1 сек. Память: 16 Мб Сложность: 28%)

---
+++ CSharp/TasksApp.Test/Tests/Task0411Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0411Test
+    {
+        private void CheckTest(int a, int b, int c, Task0411.Result result)
+        {
+            var test = Task0411.Solve(a, b, c);
+
+            Assert.AreEqual(result.Count, test.Count);
+            for (var i = 0; i < result.Roots.Length; i++)
+            {
+                Assert.AreEqual(result.Roots[i], test.Roots[i]);
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var result = new Task0411.Result(2);
+            result.Roots[0] = 2;
+            result.Roots[1] = 1;
+            
+            CheckTest(1, -3, 2, result);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            var result = new Task0411.Result(1);
+            result.Roots[0] = 6;
+            
+            CheckTest(0, -1, 6, result);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0411.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 411 - "Квадратное уравнение"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 28%)
+    /// 
+    /// Сложно найти человека, который любит решать однообразные задачки по математике.
+    /// В последнее время школьникам стало легче, ведь с появлением компьютеров почти
+    /// в каждой квартире стало существенно проще проверять себя.
+    ///
+    /// Но программы, в которых решение уравнений является стандартной функцией,
+    /// установлены не везде. Напишите программу, которая сможет решить уравнение
+    ///
+    /// ax^2 + bx + c = 0
+    ///
+    /// при заданных коэффициентах a, b и c.
+    /// 
+    /// Входные данные
+    /// Единственная строка входного файла INPUT.TXT содержит три целых числа a, b и c,
+    /// каждое из которых не превосходит по модулю 30000. Числа разделяются пробелами.
+    /// 
+    /// Выходные данные
+    /// На первой строке выходного файла OUTPUT.TXT выведите число различных действительных корней заданного уравнения.
+    /// Затем выведите сами корни по одному на строке с ошибкой, не превосходящей 10^(-4).
+    /// Если для заданных коэффициентов корней бесконечно много, на единственной строке выходного файла выведите -1.
+    /// </summary>
+    public class Task0411
+    {
+        public static void Main()
+        {
+            GetInputData(out var a, out var b, out var c);
+
+            var result = Solve(a, b, c);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int a, out int b, out int c)
+        {
+            var input = Console.ReadLine().Split();
+            
+            a = int.Parse(input[0]);
+            b = int.Parse(input[1]);
+            c = int.Parse(input[2]);
+        }
+
+        public static Result Solve(float a, float b, float c)
+        {
+            if (a == 0 && b == 0 && c == 0)
+            {
+                return new Result(-1);
+            }
+            
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    return new Result(0);
+                }
+                
+                var result = new Result(1);
+                result.Roots[0] = -c / b;
+                return result;
+            }
+
+            var d = b * b - 4 * a * c;
+            if (d > 0)
+            {
+                var x1 = (-b + (float)Math.Sqrt(d)) / (2 * a);
+                var x2 = (-b - (float)Math.Sqrt(d)) / (2 * a);
+                
+                var result = new Result(2);
+                result.Roots[0] = x1;
+                result.Roots[1] = x2;
+                return result;
+            }
+
+            if (d == 0)
+            {
+                var x1 = (-b + (float)Math.Sqrt(d)) / (2 * a);
+                
+                var result = new Result(1);
+                result.Roots[0] = x1;
+                return result;
+            }
+            
+            return new Result(0);
+        }
+
+        private static void PrintResult(Result result)
+        {
+            if (result.Count == -1)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+                
+            Console.WriteLine(result.Count);
+            foreach (var root in result.Roots)
+            {
+                Console.WriteLine(Math.Round(root, 4).ToString().Replace(',', '.'));
+            }
+        }
+        
+        public class Result
+        {
+            public int Count { get; }
+
+            public float[] Roots { get; }
+
+            public Result(int count)
+            {
+                Count = count;
+                Roots = new float[count == -1 ? 0 : count];
+            }
+        }
+    }
+}
