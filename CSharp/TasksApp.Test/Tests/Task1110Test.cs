ce4080d201e2dd87661c8f3d34104f65817042b4
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 02:28:51 +0300

    Решена задача 1110 - "Последняя цифра" (Время: 1 сек. Память: 16 Мб Сложность: 3%)

---
+++ CSharp/TasksApp.Test/Tests/Task1110Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1110Test
+    {
+        private void CheckTest(int n, int result)
+        {
+            var actual = Task1110.Solve(n);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(5, 5);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(273, 3);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1110.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1110 - "Последняя цифра"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 3%)
+	/// 
+	/// Требуется определить последнюю цифру натурального числа.
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит натуральное число, не превосходящее 10^9.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите последнюю цифру заданного числа.
+	/// </summary>
+	public static class Task1110
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+	        return n % 10;
+        }
+
+        private static void PrintResult(int result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
