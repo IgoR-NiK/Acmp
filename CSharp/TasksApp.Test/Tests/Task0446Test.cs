e68594d591151ea448983d98ea3ad56795b6fe8c
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 19:01:16 +0300

    Решена задача 446 - "Табло" (Время: 1 сек. Память: 16 Мб Сложность: 20%)

---
+++ CSharp/TasksApp.Test/Tests/Task0446Test.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0446Test
+    {
+        private void CheckTest(char[,] splash, int[,] board, string result)
+        {
+            var test = Task0446.Solve(splash, board);
+            Assert.AreEqual(result, test);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new[,]
+            {
+                { '.', 'G', 'B' },
+                { 'R', '.', 'B' },
+                { 'R', 'G', '.' }
+            }, new[,]
+            {
+                { 0, 1, 2 },
+                { 3, 4, 5 },
+                { 6, 7, 0 }
+            }, "NO");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new[,]
+            {
+                { 'R', 'G', 'B' },
+                { '.', 'G', '.' }
+            }, new[,]
+            {
+                { 7, 7, 7 },
+                { 7, 7, 7 }
+            }, "YES");
+        }
+    }
+}
