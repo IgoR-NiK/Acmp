197f9ce75c7f7fa6b2ec2b7053b5ed8271a87197
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 02:40:17 +0300

    Решена задача 1113 - "Проверьте делимость" (Время: 1 сек. Память: 16 Мб Сложность: 10%)

---
+++ CSharp/TasksApp.Test/Tests/Task1113Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1113Test
+    {
+        private void CheckTest(int a, int b, bool result)
+        {
+            var actual = Task1113.Solve(a, b);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(3, 6, true);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(6, 3, true);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(5, 7, false);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1113.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1113 - "Проверьте делимость"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 10%)
+	/// 
+	/// Даны два натуральных числа. Требуется проверить: делится ли одно из них на другое?
+	/// 
+	/// Входные данные
+	/// Первая строка входного файла INPUT.TXT содержит два натуральных числа, разделенных пробелом.
+	/// Числа не превосходят 100.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите 1, если одно из чисел делится на другое нацело.
+	/// В противном случае следует вывести любое другое натуральное число, отличное от 1 и не превосходящее 10^9.
+	/// </summary>
+	public static class Task1113
+    {
+        public static void Main()
+        {            
+            GetInputData(out var a, out var b);
+
+            var result = Solve(a, b);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int a, out int b)
+        {
+	        var input = Console.ReadLine().Split();
+	        a = int.Parse(input[0]);
+	        b = int.Parse(input[1]);
+        }
+
+        public static bool Solve(int a, int b)
+        {
+	        return a % b == 0 || b % a == 0;
+        }
+
+        private static void PrintResult(bool result)
+        {
+	        Console.WriteLine(result ? 1 : 2);
+        }
+    }
+}
