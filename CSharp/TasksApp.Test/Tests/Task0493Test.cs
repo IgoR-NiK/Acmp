545b856db3ba93963b426bb240ac840c34f4da2b
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 22:20:16 +0300

    Решена задача 493 - "Морской бой - 2" (Время: 1 сек. Память: 16 Мб Сложность: 21%)

---
+++ CSharp/TasksApp.Test/Tests/Task0493Test.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0493Test
+    {
+        private void CheckTest(char[,] field, int result)
+        {
+            var actual = Task0493.Solve(field);
+            
+            Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new [,]
+            {
+                { '*', '*', '*', '*' },
+                { '*', '*', '.', '.' },
+                { '*', '.', '.', '.' },
+                { '*', '.', '.', '.' }
+            }, 4);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new [,]
+            {
+                { '*', '*', '*' },
+                { '.', '.', '.' },
+                { '.', '.', '.' },
+                { '*', '*', '*' }
+            }, 0);
+        }
+    }
+}
