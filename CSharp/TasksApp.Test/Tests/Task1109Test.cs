873d05115e0d1d1d4470fe3fc1be1fef4e389a14
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 02:25:15 +0300

    Решена задача 1109 - "Следующее и предыдущее" (Время: 1 сек. Память: 16 Мб Сложность: 4%)

---
+++ CSharp/TasksApp.Test/Tests/Task1109Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1109Test
+    {
+        private void CheckTest(int n, string[] results)
+        {
+            var actual = Task1109.Solve(n);
+			Assert.AreEqual(results.Length, actual.Length);
+
+            for (var i = 0; i < results.Length; i++)
+            {
+                Assert.AreEqual(results[i], actual[i]);
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(13, new[]
+            {
+                "The next number for the number 13 is 14.",
+                "The previous number for the number 13 is 12."
+            });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1109.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1109 - "Следующее и предыдущее"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 4%)
+	/// 
+	/// Напишите программу, которая считывает целое число и выводит текст с упоминанием следующего и предыдущего для него чисел.
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит целое число, не превосходящее 1000 по абсолютной величине.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите текст, аналогичный приведенному в примере.
+	/// При выводе следует учесть все пробелы и знаки препинания.
+	/// </summary>
+	public static class Task1109
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static string[] Solve(int n)
+        {
+	        return new[]
+	        {
+		        $"The next number for the number {n} is {n + 1}.",
+		        $"The previous number for the number {n} is {n - 1}."
+	        };
+        }
+
+        private static void PrintResult(string[] results)
+        {
+	        foreach (var result in results)
+	        {
+		        Console.WriteLine(result);
+	        }
+        }
+    }
+}
