c81985b7342951989a6c95b6c925d35a4821d4de
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 02:31:38 +0300

    Решена задача 1111 - "Число десятков" (Время: 1 сек. Память: 16 Мб Сложность: 5%)

---
+++ CSharp/TasksApp.Test/Tests/Task1111Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1111Test
+    {
+        private void CheckTest(int n, int result)
+        {
+            var actual = Task1111.Solve(n);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(123, 2);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(46, 4);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1111.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1111 - "Число десятков"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 5%)
+	/// 
+	/// Требуется определить число десятков в заданном натуральном числе
+	/// в его десятичной записи (то есть найти предпоследнюю цифру в числе).
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит натуральное число, не превосходящее 10^9.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите ответ на задачу.
+	/// </summary>
+	public static class Task1111
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+	        return (n / 10) % 10;
+        }
+
+        private static void PrintResult(int result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
