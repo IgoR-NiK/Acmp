8079429256fc548dce03dabcf0282b37762d6542
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 22:05:43 +0300

    Решена задача 466 - "Функция" (Время: 1 сек. Память: 16 Мб Сложность: 42%)

---
+++ CSharp/TasksApp.Test/Tests/Task0466Test.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0466Test
+    {
+        private void CheckTest(int n, int result)
+        {
+            var actual = Task0466.Solve(n);
+            
+            Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(2, 1);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(5, 3);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0466.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 466 - "Функция"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 42%)
+    /// 
+    /// Функция f(n) определена следующим образом:
+    ///
+    /// f(0)=0, f(1)=1, f(2n)=f(n), f(2n+1)=f(n)+f(n+1).
+    ///
+    /// Требуется написать программу, которая по заданному натуральному числу N определяет значение функции f(N).
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит число N (1 ≤ N ≤ 2147483647).
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите значение f(N).
+    /// </summary>   
+    public class Task0466
+    {
+        public static void Main()
+        {
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+            if (n < 2) 
+                return n;
+            
+            return n % 2 == 1 ? Solve(n / 2) + Solve(n / 2 + 1) : Solve(n / 2);
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
