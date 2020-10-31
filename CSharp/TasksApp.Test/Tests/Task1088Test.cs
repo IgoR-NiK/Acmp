4723d84ada222df77c8498085b40c4d71113ec5f
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 22:54:24 +0300

    Решена задача 1088 - "Шахматные фигуры" (Время: 1 сек. Память: 16 Мб Сложность: 27%)

---
+++ CSharp/TasksApp.Test/Tests/Task1088Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1088Test
+    {
+        private void CheckTest(string point1, string point2, string[] results)
+        {
+            var actual = Task1088.Solve(point1, point2);
+			Assert.AreEqual(results.Length, actual.Length);
+
+            foreach (var result in results)
+            {
+                Assert.True(actual.Contains(result));
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest("D3", "D7", new[] { "Queen", "Rook" });
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest("F4", "H5", new[] { "Knight" });
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest("E2", "E3", new[] { "King", "Queen", "Rook", "Pawn" });
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            CheckTest("A1", "H7", new[] { "Nobody" });
+        }
+    }
+}
