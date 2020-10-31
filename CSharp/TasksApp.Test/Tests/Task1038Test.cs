82d62c2049f9bff02d55b5013a627fe357fa92b8
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 23:42:45 +0300

    Решена задача 1038 - "Перевязь" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task1038Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1038Test
+    {
+        private void CheckTest(int n, int result)
+        {
+            var actual = Task1038.Solve(n);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(200, 20);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(203, 21);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1038.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1038 - "Перевязь"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 12%)
+	/// 
+	/// Портос хочет украсить золотым шитьем свою перевязь. Он знает, что один сантиметр золотого шитья стоит один луидор.
+	/// Портосу надо вышить N миллиметров перевязи. Причем мастер никогда не возьмется за работу,
+	/// если ему заплатят меньше, чем стоит работа. И сдачу мастер никогда не отдает.
+	///
+	/// Какое минимальное количество луидоров Портос должен заплатить мастеру за работу?
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит натуральное число N (N ≤ 10^9) – длина перевязи в миллиметрах.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите минимальное количество луидоров, которые Портос должен отдать за работу.
+	/// </summary>
+	public static class Task1038
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+	        n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+	       return (int)Math.Ceiling(n / 10.0);
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
