020d0de35ee7abc0a02c8566d7ef870c6873431f
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 23:17:21 +0300

    Решена задача 1006 - "Светофор" (Время: 1 сек. Память: 16 Мб Сложность: 10%)

---
+++ CSharp/TasksApp.Test/Tests/Task1006Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1006Test
+    {
+        private void CheckTest(string input, string result)
+        {
+            var actual = Task1006.Solve(input);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest("blackblackgreen", "black\nblack\nGREEN");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest("blackYELLOWblack", "black\nYELLOW\nblack");
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest("redyellowgreen", "error");
+        }
+    }
+}
