5336cef38bd869a73d3b400ea5344f4b5a0c8607
Igor Nikolaev <Igor_2501@mail.ru>
2020-10-31 22:01:47 +0300

    Решена задача 461 - "Выборы" (Время: 1 сек. Память: 16 Мб Сложность: 26%)

---
+++ CSharp/TasksApp.Test/Tests/Task0461Test.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0461Test
+    {
+        private void CheckTest(int[] input, int result)
+        {
+            var actual = Task0461.Solve(input);
+            
+            Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new[] { 5, 5, 7 }, 6);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new[] { 4, 2, 1, 3, 7 }, 5);
+        }
+    }
+}
