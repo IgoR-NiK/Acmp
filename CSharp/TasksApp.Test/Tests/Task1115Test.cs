ae7a24cd362379f977f98666da428dbee35b5408
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 02:55:03 +0300

    Решена задача 1115 - "Дележ яблок" (Время: 1 сек. Память: 16 Мб Сложность: 115)

---
+++ CSharp/TasksApp.Test/Tests/Task1115Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1115Test
+    {
+        private void CheckTest(int n, int k, (int a, int b, int c) result)
+        {
+            var actual = Task1115.Solve(n, k);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(7, 30, (4, 2, 5));
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(5, 15, (3, 0, 0));
+        }
+    }
+}
