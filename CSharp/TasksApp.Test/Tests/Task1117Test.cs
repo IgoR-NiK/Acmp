6fd4c9ebf3a6296f5ccb9a536e8e945349a69d99
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 03:19:22 +0300

    Решена задача 1117 - "Конец уроков" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task1117Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1117Test
+    {
+        private void CheckTest(int n, (int h, int m) result)
+        {
+            var actual = Task1117.Solve(n);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(2, (10, 35));
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(3, (11, 35));
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1117.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1117 - "Конец уроков"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 12%)
+	/// 
+	/// В некоторой школе занятия начинаются в 9:00. Продолжительность урока – 45 минут,
+	/// после 1-го, 3-го, 5-го и т.д. уроков перемена длится 5 минут, а после 2-го, 4-го, 6-го и т.д. – 15 минут.
+	///
+	/// По номеру урока требуется определить время его окончания.
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит целое число от 1 до 10 – номер урока.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите два целых числа: время окончания урока в часах и минутах.
+	/// </summary>
+	public static class Task1117
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static (int h, int m) Solve(int n)
+        {
+	        var m = n * 45;
+	        while (n != 1)
+	        {
+		        n--;
+		        m += n % 2 == 0 ? 15 : 5;
+	        }
+
+	        return (9 + m / 60, m % 60);
+        }
+
+        private static void PrintResult((int h, int m) result)
+        {
+	        Console.WriteLine($"{result.h} {result.m}");
+        }
+    }
+}
