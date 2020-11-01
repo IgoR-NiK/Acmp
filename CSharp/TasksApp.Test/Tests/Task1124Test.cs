7a7635a2f13dc394eddc2cd31d1aac235f3ba643
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 03:49:17 +0300

    Решена задача 1124 - "Пешка" (Время: 1 сек. Память: 16 Мб Сложность: 18%)

---
+++ CSharp/TasksApp.Test/Tests/Task1124Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1124Test
+    {
+        private void CheckTest(int x1, int y1, int x2, int y2, string result)
+        {
+            var actual = Task1124.Solve(x1, y1, x2, y2);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(5, 2, 5, 4, "YES");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(5, 6, 5, 7, "YES");
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(2, 3, 3, 4, "NO");
+        }
+    }
+}
