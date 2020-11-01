1c4ddf40024900f6eda5edf916aba076a616edc4
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 04:34:00 +0300

    Решена задача 1132 - "Длина последовательности" (Время: 1 сек. Память: 16 Мб Сложность: 11%)

---
+++ CSharp/TasksApp.Test/Tests/Task1132Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1132Test
+    {
+        private void CheckTest(string[] input, int result)
+        {
+            var actual = Task1132.Solve(input);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new[] { "1", "7", "9", "0", "5" }, 3);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new[] { "1", "2", "3", "4", "5", "0", "6", "7", "0", "8", "9", "10" }, 5);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(new[] { "0" }, 0);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1132.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1132 - "Длина последовательности"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 11%)
+	/// 
+	/// Задана последовательность неотрицательных целых чисел. Требуется определить длину последовательности,
+	/// завершающуюся первым встретившимся нолем (число 0 не входит в последовательность).
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит последовательность неотрицательных целых чисел, не превышающих значения 100.
+	/// Гарантируется, что во входных данных не более 100 чисел и среди них есть хотя бы одно число 0.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите целое число – длину последовательности.
+	/// </summary>
+	public static class Task1132
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+
+            var result = Solve(input);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out string[] input)
+        {
+	        input = Console.ReadLine().Split();
+        }
+
+        public static int Solve(string[] input)
+        {
+	        var result = 0;
+	        
+	        foreach (var n in input)
+	        {
+		        if (n == "0")
+			        break;
+
+		        result++;
+	        }
+
+	        return result;
+        }
+
+        private static void PrintResult(int result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
