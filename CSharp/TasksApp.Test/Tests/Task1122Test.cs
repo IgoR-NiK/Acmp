c7bc90793d05f60a94e29ac5538bf71698aff489
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 03:42:01 +0300

    Решена задача 1122 - "Король" (Время: 1 сек. Память: 16 Мб Сложность: 11%)

---
+++ CSharp/TasksApp.Test/Tests/Task1122Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1122Test
+    {
+        private void CheckTest(int x1, int y1, int x2, int y2, string result)
+        {
+            var actual = Task1122.Solve(x1, y1, x2, y2);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(3, 3, 4, 3, "YES");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(3, 3, 5, 3, "NO");
+        }
+    }
+}
