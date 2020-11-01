444161b1aba2bfcfc1d7e70573c012fc64e3aad3
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 15:38:19 +0300

    Решена задача 1141 - "Равные элементы" (Время: 1 сек. Память: 16 Мб Сложность: 19%)

---
+++ CSharp/TasksApp.Test/Tests/Task1141Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1141Test
+    {
+        private void CheckTest(string[] input, int result)
+        {
+            var actual = Task1141.Solve(input);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new[] { "1", "7", "7", "9", "1", "0" }, 2);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new[] { "1", "2", "3", "3", "3", "3", "4", "5", "3", "4", "0", "5" }, 4);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(new[] { "0" }, 1);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1141.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1141 - "Равные элементы"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 19%)
+	/// 
+	/// Дана последовательность натуральных чисел, завершающаяся числом 0.
+	/// Требуется определить, какое наибольшее число подряд идущих элементов этой последовательности равны друг другу.
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит последовательность неотрицательных целых чисел, не превышающих значения 100.
+	/// Гарантируется, что во входных данных не более 100 чисел и среди них есть хотя бы одно число 0.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите целое число – ответ на задачу.
+	/// </summary>
+	public static class Task1141
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+
+            var result = Solve(input);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out string[] input)
+        {
+	        input = Console.ReadLine().Split();
+        }
+
+        public static int Solve(string[] input)
+        {
+	        int i = 1, a1, a2, t = 0, max = 0;
+	        a1 = int.Parse(input[0]);
+	        while (a1 != 0)
+	        {
+		        a2 = int.Parse(input[i]);
+		        if (a2 == a1)
+		        {
+			        t++;
+			        if (t > max) max = t;
+		        }
+		        else
+		        {
+			        t = 0;
+		        }
+  
+		        a1 = a2;
+  
+		        i++;
+	        }
+ 
+	        return max + 1;
+        }
+
+        private static void PrintResult(int result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
