563928d008469a7c8dc71e56f751fe374017d2e0
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 15:43:10 +0300

    Решена задача 1146 - "Цифра" (Время: 1 сек. Память: 16 Мб Сложность: 6%)

---
+++ CSharp/TasksApp.Test/Tests/Task1146Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1146Test
+    {
+        private void CheckTest(char input, string result)
+        {
+            var actual = Task1146.Solve(input);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest('5', "Yes");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest('A', "No");
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1146.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1146 - "Цифра"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 6%)
+	/// 
+	/// Задан символ. Требуется определить: является ли он цифрой?
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит единственный символ (ASCII-код от 33 до 127).
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите «Yes», если символ является цифрой, и «No» в противном случае.
+	/// </summary>
+	public static class Task1146
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+
+            var result = Solve(input);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out char input)
+        {
+	        input = char.Parse(Console.ReadLine());
+        }
+
+        public static string Solve(char input)
+        {
+	        return char.IsDigit(input) ? "Yes" : "No";
+        }
+
+        private static void PrintResult(string result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
