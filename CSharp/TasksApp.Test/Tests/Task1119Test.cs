77ca7558c970d86d962a45f6dcc0048f50bb7ff4
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 03:33:21 +0300

    Решена задача 1119 - "Ладья" (Время: 1 сек. Память: 16 Мб Сложность: 8%)

---
+++ CSharp/TasksApp.Test/Tests/Task1119Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1119Test
+    {
+        private void CheckTest(int x1, int y1, int x2, int y2, string result)
+        {
+            var actual = Task1119.Solve(x1, y1, x2, y2);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(4, 3, 7, 3, "YES");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(4, 3, 6, 1, "NO");
+        }
+    }
+}
