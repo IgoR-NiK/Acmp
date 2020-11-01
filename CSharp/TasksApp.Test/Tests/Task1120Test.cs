8e678dd77c252679af2bf74cfe5314f0229f3df4
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 03:36:06 +0300

    Решена задача 1120 - "Слон" (Время: 1 сек. Память: 16 Мб Сложность: 13%)

---
+++ CSharp/TasksApp.Test/Tests/Task1120Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1120Test
+    {
+        private void CheckTest(int x1, int y1, int x2, int y2, string result)
+        {
+            var actual = Task1120.Solve(x1, y1, x2, y2);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(5, 4, 7, 2, "YES");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(5, 4, 4, 6, "NO");
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1120.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1120 - "Слон"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 13%)
+	/// 
+	/// Напомним, что в шахматах используется клеточная доска размером 8х8, где располагаются шахматные фигуры,
+	/// которые могут перемещаться по определенным правилам.
+	/// В частности, слон может перемещаться на любое расстояние по диагонали.
+	///
+	/// Требуется определить: может ли слон выполнить ход из клетки с координатами (X1,Y1)
+	/// в клетку с координатами (X2,Y2) на стандартной шахматной доске?
+	/// 
+	/// Входные данные
+	/// Первая строка входного файла INPUT.TXT содержит два целых числа X1 и Y1 – начальную координату слона.
+	/// Во второй строке записана конечная координата – целые числа X2 и Y2 (1 ≤ X1,Y1,X2,Y2 ≤ 8).
+	/// Гарантируется, что начальная и конечная координаты не совпадают.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите «YES», если ход является допустимым, и «NO» - в противном случае.
+	/// </summary>
+	public static class Task1120
+    {
+        public static void Main()
+        {            
+            GetInputData(out var x1, out var y1, out var x2, out var y2);
+
+            var result = Solve(x1, y1, x2, y2);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int x1, out int y1, out int x2, out int y2)
+        {
+	        var input = Console.ReadLine().Split();
+	        x1 = int.Parse(input[0]);
+	        y1 = int.Parse(input[1]);
+	        
+	        input = Console.ReadLine().Split();
+	        x2 = int.Parse(input[0]);
+	        y2 = int.Parse(input[1]);
+        }
+
+        public static string Solve(int x1, int y1, int x2, int y2)
+        {
+	        return Math.Abs(x1 - x2) == Math.Abs(y1 - y2) ? "YES" : "NO";
+        }
+
+        private static void PrintResult(string result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
