f262ad5fbec31df74dd5a315f1b1e5f84806233e
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 15:48:01 +0300

    Решена задача 1152 - "Вставка символов" (Время: 1 сек. Память: 16 Мб Сложность: 14%)

---
+++ CSharp/TasksApp.Test/Tests/Task1152Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1152Test
+    {
+        private void CheckTest(string input, string result)
+        {
+            var actual = Task1152.Solve(input);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest("VisualC", "V#i#s#u#a#l#C");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest("INSERT", "I#N#S#E#R#T");
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1152.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1152 - "Вставка символов"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 14%)
+	/// 
+	/// Дана строка. Требуется между ее символами вставить символ «#» (ASCII 35).
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит строку, состоящую только из английских букв. Строка содержит от 1 до 100 букв.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите строку, полученную из исходной, путем вставки символа «#» между всеми ее буквами.
+	/// </summary>
+	public static class Task1152
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+
+            var result = Solve(input);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out string input)
+        {
+	        input = Console.ReadLine();
+        }
+
+        public static string Solve(string input)
+        {
+	        for (var i = 1; i < input.Length; i += 2)
+		        input = input.Insert(i, "#");
+
+	        return input;
+        }
+
+        private static void PrintResult(string result)
+        {
+	        Console.WriteLine(result);
+        }
+    }
+}
