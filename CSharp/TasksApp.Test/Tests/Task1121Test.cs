7525739c9fae64ad42eae48424bfa3b9d9c6bdba
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 03:39:30 +0300

    Решена задача 1121 - "Ферзь" (Время: 1 сек. Память: 16 Мб Сложность: 14%)

---
+++ CSharp/TasksApp.Test/Tests/Task1121Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1121Test
+    {
+        private void CheckTest(int x1, int y1, int x2, int y2, string result)
+        {
+            var actual = Task1121.Solve(x1, y1, x2, y2);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(5, 4, 5, 8, "YES");
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(5, 4, 7, 6, "YES");
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(1, 1, 2, 3, "NO");
+        }
+    }
+}
