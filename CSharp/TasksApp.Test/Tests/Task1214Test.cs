74a95fc89418b28496021412c078b782c9e55b5a
Igor Nikolaev <Igor_2501@mail.ru>
2020-11-01 15:59:13 +0300

    Решена задача 1214 - "Линейный поиск" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task1214Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1214Test
+    {
+        private void CheckTest(int[] numbers, int search, int result)
+        {
+            var actual = Task1214.Solve(numbers, search);
+			Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new[] { 1, 2, 3, 4, 5 }, 3, 1);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new[] { 5, 3, 4, 5, 5, 2, 1 }, 5, 3);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1214.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1214 - "Линейный поиск"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 12%)
+	/// 
+	/// Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+	/// 
+	/// Входные данные
+	/// Первая строка входного файла INPUT.TXT содержит натуральное число N – количество элементов в массиве.
+	/// Во второй строке записаны N целых чисел A(i), разделенные пробелом.
+	/// Третья строка содержит число X. Все числа во входных данных не превосходят 1000 по абсолютной величине.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите количество вхождений числа X в массив A.
+	/// </summary>
+	public static class Task1214
+    {
+        public static void Main()
+        {            
+            GetInputData(out var numbers, out var search);
+
+            var result = Solve(numbers, search);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int[] numbers, out int search)
+        {
+	        var n = int.Parse(Console.ReadLine());
+	        var str = Console.ReadLine().Split();
+
+	        numbers = new int[n];
+	        for (var i = 0; i < n; i++)
+		        numbers[i] = int.Parse(str[i]);
+
+	        search = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int[] numbers, int search)
+        {
+	        var result = 0;
+
+	        for (var i = 0; i < numbers.Length; i++)
+	        {
+		        if (search == numbers[i]) 
+			        result++;
+	        }
+
+	        return result;
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
