f73f2f2d2057959788a4af4a42fc7b864e2ce599
Igor Nikolaev <IgoR_2501@mail.ru>
2022-02-23 02:29:06 +0300

    Решена задача 349 - "Простые числа" (Время: 0,5 сек. Память: 32 Мб Сложность: 28%)

---
+++ CSharp/TasksApp.Test/Tests/Task0349Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0349Test
+    {
+        private void CheckTest(int m, int n, List<int> expected)
+        {
+            var actual = Task0349.Solve(m, n).ToList();
+            
+            Assert.AreEqual(expected.Count, actual.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(2, 5, new List<int> { 2, 3, 5 });
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(4, 4, new List<int>());
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0349.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 349 - "Простые числа"
+    /// (Время: 0,5 сек. Память: 32 Мб Сложность: 28%)
+    /// 
+    /// Необходимо вывести все простые числа от M до N включительно.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит два натуральных числа M и N, разделенных пробелом (2 ≤ M ≤ N ≤ 10^6)
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите все простые числа от M до N в порядке возрастания, по одному в строке. Если таковых чисел нет, то следует вывести «Absent».
+    /// </summary>
+    public static class Task0349
+    {
+        private static readonly List<int> _primeNumbers = new List<int> { 2 };
+
+        public static void Main()
+        {
+            GetInputData(out var m, out var n);
+
+            var result = Solve(m, n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int m, out int n)
+        {
+            var input = Console.ReadLine().Split();
+
+            m = int.Parse(input[0]);
+            n = int.Parse(input[1]);
+        }
+
+        public static IEnumerable<int> Solve(int m, int n)
+        {
+            if (m == 2)
+            {
+                yield return m;
+            }             
+
+            for (var i = 3; i <= n; i += 2)
+            {
+                if (IsPrime(i) && i >= m)
+                {
+                    yield return i;
+                }                
+            }
+        }
+
+        private static void PrintResult(IEnumerable<int> result)
+        {
+            if (!result.Any())
+            {
+                Console.Write("Absent");
+                return;                
+            }
+
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (var j = 0; _primeNumbers[j] * _primeNumbers[j] <= number; j++)
+            {
+                if (number % _primeNumbers[j] == 0)
+                {
+                    return false;
+                }
+            }
+
+            _primeNumbers.Add(number);
+            return true;
+        }
+
+        private static bool Any<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                return enumerator.MoveNext();
+            }
+        }
+    }
+}
