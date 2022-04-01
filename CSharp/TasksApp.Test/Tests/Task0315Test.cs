af1ddfcf893a8bd51e60e68a8b1e7a7ae4461abe
Igor Nikolaev <IgoR_2501@mail.ru>
2022-04-02 01:47:16 +0300

    Решена задача 315 - "Наименьшая система счисления" (Время: 1 сек. Память: 16 Мб Сложность: 26%)

---
+++ CSharp/TasksApp.Test/Tests/Task0315Test.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0315Test
+    {
+        private void CheckTest(string n, int result)
+        {
+            var solution = Task0315.Solve(n);
+
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest("123", 4);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest("ABCDEF", 16);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            CheckTest("AD%AF", -1);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            CheckTest("03025", 6);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            CheckTest("abc", -1);
+        }
+    }
+}
