cb1305f247d3903f2c7fd4be270c5d22bbed0271
Igor Nikolaev <IgoR_2501@mail.ru>
2022-07-29 20:35:30 +0300

    Решена задача 121 - "Гвоздики" (Время: 1 сек. Память: 16 Мб Сложность: 34%)

---
+++ CSharp/TasksApp.Test/Tests/Task0121Test.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+	[TestFixture]
+	public class Task0121Test
+	{
+		private static void CheckTest(int[] input, int result)
+		{
+			var resultTest = Task0121.Solve(input);
+			Assert.AreEqual(result, resultTest);
+		}
+
+		[Test]
+		public void Test1()
+		{
+			CheckTest(new[] { 3, 4, 12, 6, 14, 13 }, 5);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(new[] { 10, 3, 7, 19, 20, 21 }, 9);
+		}
+
+		[Test]
+		public void Test3()
+		{
+			CheckTest(new[] { 3, 7, 10, 15, 20, 21 }, 10);
+		}
+
+		[Test]
+		public void Test4()
+		{
+			CheckTest(new[] { 2, 3, 6, 8, 9, 12, 15 }, 7);
+		}
+
+		[Test]
+		public void Test5()
+		{
+			CheckTest(new[] { 682, 2517, 2478, 2816, 4980, 5839, 6414, 7667, 8802, 8995 }, 4400);
+		}
+
+		[Test]
+		public void Test6()
+		{
+			CheckTest(new[] { 593, 716, 845, 603, 858, 545, 848, 424, 624, 646, 385, 438, 298, 892, 57, 964, 273, 384, 478, 792, 813, 529, 480, 569, 393, 926, 837, 72, 338 }, 340);
+		}
+
+		[Test]
+		public void Test7()
+		{
+			CheckTest(new[] { 69, 73 }, 4);
+		}
+
+		[Test]
+		public void Test8()
+		{
+			CheckTest(new[] { 123, 69, 73 }, 54);
+		}
+
+		[Test]
+		public void Test9()
+		{
+			CheckTest(new[] { 1, 2, 15, 17 }, 3);
+		}
+
+		[Test]
+		public void Test10()
+		{
+			CheckTest(new[] { 1, 2, 10, 15, 17 }, 8);
+		}
+
+		[Test]
+		public void Test11()
+		{
+			CheckTest(new[] { 0, 1000 }, 1000);
+		}
+
+		[Test]
+		public void Test12()
+		{
+			CheckTest(new[] { 10, 100 }, 90);
+		}
+	}
+}
---
+++ CSharp/TasksApp/Tasks/Task0121.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 121 - "Гвоздики"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 34%)
+	/// 
+	/// На прямой дощечке вбиты гвоздики. Любые два гвоздика можно соединить ниточкой. Требуется соединить некоторые пары гвоздиков ниточками так,
+	/// чтобы к каждому гвоздику была привязана хотя бы одна ниточка, а суммарная длина всех ниточек была минимальна.
+	/// 
+	/// Входные данные
+	/// В первой строке входного файла INPUT.TXT записано число N - количество гвоздиков (2 ≤ N ≤ 100). 
+	/// В следующей строке записано N чисел - координаты всех гвоздиков (неотрицательные целые числа, не превосходящие 10000).
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT нужно вывести единственное число - минимальную суммарную длину всех ниточек.
+	/// </summary>
+	public class Task0121
+    {
+        public static void Main()
+        {            
+            GetInputData(out var input);
+
+            var result = Solve(input);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int[] input)
+        {
+			var n = int.Parse(Console.ReadLine());
+			input = new int[n];
+
+			var str = Console.ReadLine().Split();
+			for (var i = 0; i < n; i++)
+			{
+				input[i] = int.Parse(str[i]);
+			}
+        }
+
+        public static int Solve(int[] input)
+        {
+			var n = input.Length;
+
+			Array.Sort(input);
+
+			var mins = new int[n];
+
+			mins[0] = int.MaxValue;
+			mins[1] = input[1] - input[0];
+
+			for (var i = 2; i < n; i++)
+            {
+				mins[i] = Math.Min(mins[i - 1], mins[i - 2]) + input[i] - input[i - 1];
+            }
+
+			return mins[n - 1];
+		}
+
+        private static void PrintResult(int result)
+        {
+			Console.Write(result);
+        }
+	}
+}
