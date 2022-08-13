2966de0e9b2c12bf73efe7e213d6e38f1c093fb1
Igor Nikolaev <IgoR_2501@mail.ru>
2022-08-14 02:12:07 +0300

    Решена задача 1125 - "Список квадратов" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task1125Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task1125Test
+    {
+        private static void CheckTest(int n, int[] result)
+        {
+            var actual = Task1125.Solve(n).ToArray();
+			Assert.AreEqual(result.Length, actual.Length);
+
+            for (var i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(result[i], actual[i]);
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(10, new[] { 1, 4, 9 });
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(50, new[] { 1, 4, 9, 16, 25, 36, 49 });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task1125.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 1125 - "Список квадратов"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 12%)
+	/// 
+	/// Требуется вывести квадраты натуральных чисел, не превосходящие числа N.
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит натуральное число N (N ≤ 10^9).
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите все квадраты натуральных чисел, не превосходящие N, в порядке возрастания.
+	/// </summary>
+	public static class Task1125
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+	        n = int.Parse(Console.ReadLine());
+        }
+
+        public static IEnumerable<int> Solve(int n)
+        {
+	        int x = 1, i = 1;
+	        while (x <= n)
+	        {
+		        yield return x;
+		        
+		        i++;
+		        x = i * i;
+	        }
+        }
+
+        private static void PrintResult(IEnumerable<int> result)
+        {
+	        foreach (var number in result)
+	        {
+		        Console.Write($"{number} ");
+	        }
+        }
+    }
+}
