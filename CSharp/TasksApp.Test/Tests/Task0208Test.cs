996d1dd085d95611fb3ef19506a788f2da0614f0
Igor Nikolaev <IgoR_2501@mail.ru>
2022-08-28 21:10:51 +0300

    Решена задача 208 - "Забавная игра" (Время: 1 сек. Память: 16 Мб Сложность: 30%)

---
+++ CSharp/TasksApp.Test/Tests/Task0208Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0208Test
+    {
+        private static void CheckTest(int n, int result)
+        {
+            var solution = Task0208.Solve(n);
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(19, 28);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(1212, 1938);
+        }
+    }
+}
