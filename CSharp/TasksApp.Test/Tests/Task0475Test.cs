4124a7f6bcfe406fd0e9975ec71c1d0369017915
Igor Nikolaev <IgoR_2501@mail.ru>
2022-08-31 01:39:45 +0300

    Решена задача 475 - "Арифметическая прогрессия - 2" (Время: 1 сек. Память: 16 Мб Сложность: 38%)

---
+++ CSharp/TasksApp.Test/Tests/Task0475Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task075Test
+    {
+        private static void CheckTest(IEnumerable<int> numbers, bool result)
+        {
+            var actual = Task0475.Solve(numbers);
+            
+            Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(new[] { 80, 50, 10, 30, 70, 40, 20, 60, 90 }, true);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(new[] { 1, 2, 3, 5 }, false);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0475.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 475 - "Арифметическая прогрессия - 2"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 38%)
+    /// 
+    /// Задана последовательность натуральных чисел из диапазона [1, 2147483647].
+    /// Необходимо определить: можно ли выстроить эти числа в отрезок арифметической прогрессии.
+    /// При необходимости порядок чисел в последовательности можно изменять.
+    ///
+    /// Требуется написать программу для решения этой задачи.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит последовательность натуральных чисел.
+    /// Количество чисел в последовательности может быть от 2 до 100 000.
+    /// Числа в файле разделены пробелами или символами перехода на новую строку.
+    /// 
+    /// Выходные данные
+    /// Выходной файл OUTPUT.TXT должен содержать либо «Yes» в случае положительного ответа, либо «No» в противоположном случае.
+    /// </summary>   
+    public class Task0475
+    {
+        public static void Main()
+        {
+            var numbers = GetInputData();
+
+            var result = Solve(numbers);
+
+            PrintResult(result);
+        }
+
+        private static IEnumerable<int> GetInputData()
+        {
+            while (TryReadInt(out var number))
+            {
+                yield return number;
+            }
+        }
+
+        public static bool Solve(IEnumerable<int> numbers)
+        {
+            var array = new int[100_000];
+
+            var n = 0;
+            foreach (var number in numbers)
+            {
+                array[n] = number;
+                n++;
+            }
+            
+            Array.Sort(array, 0, n);
+
+            var d = array[1] - array[0];
+            for (var i = 2; i < n; i++)
+            {
+                if (array[i] - array[i - 1] != d)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void PrintResult(bool result)
+        {
+            Console.Write(result ? "Yes" : "No");
+        }
+
+        private static bool TryReadInt(out int result)
+        {
+            result = 0;
+
+            while (true)
+            {
+                var symbol = Console.Read();
+
+                if (symbol == -1)
+                {
+                    return result != 0;
+                }
+                
+                if (symbol >= '0' && symbol <= '9')
+                {
+                    result = result * 10 + (symbol - '0');
+                    continue;
+                }
+
+                if (result != 0)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
