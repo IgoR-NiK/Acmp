8f74da701759d727f0f048f3d624d8d1977f19d1
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 01:59:45 +0300

    Решена задача 514 - "Лестница" (Время: 1 сек. Память: 16 Мб Сложность: 14%)

---
+++ CSharp/TasksApp.Test/Tests/Task0514Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0514Test
+    {
+        private static void CheckTest(int k, int result)
+        {
+            var actual = Task0514.Solve(k);
+            Assert.AreEqual(result, actual);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(1, 1);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            CheckTest(4, 2);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(6, 3);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0514.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 514 - "Лестница"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 14%)
+	/// 
+	/// Мальчик Петя строит из кубиков лестницу.
+	/// Лестница представляет собой несколько строящихся рядом башенок из кубиков, каждая из которых ровно на один кубик выше предыдущей.
+	/// Требуется по имеющемуся у мальчика Пети числу кубиков определить, какой в кубиках будет высота последней ступеньки.
+	/// 
+	/// Входные данные
+	/// Входной файл INPUT.TXT содержит число К – количество кубиков у мальчика Пети (1 ≤ K ≤ 10^6).
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите количество кубиков в последней ступеньке у максимально высокой лестницы, которую можно построить из K кубиков.
+	/// </summary>
+	public static class Task0514
+    {
+        public static void Main()
+        {            
+            GetInputData(out var k);
+
+            var result = Solve(k);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int k)
+        {
+	        k = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int k)
+        {
+	        return (int)((-1 + Math.Sqrt(1 + 8 * k)) / 2);
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
