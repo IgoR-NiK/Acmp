a6218aed3908b4ce532775169b06615b8b9c8ddf
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 02:05:45 +0300

    Решена задача 23 - "Гадание" (Время: 1 сек. Память: 16 Мб Сложность: 13%)

---
+++ CSharp/TasksApp.Test/Tests/Task0023Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0023Test
+    {
+        private static void CheckTest(int n, int output)
+        {
+            var test = Task0023.Solve(n);
+            Assert.AreEqual(test, output);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(6, 12);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(10, 18);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0023.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 23 - "Гадание"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 13%)
+    /// 
+    /// Как и многие другие девочки, Маша любит разные гадания.
+    /// Некоторое время назад Маша узнала новый способ гадать на числах – для какого-нибудь
+    /// интересующего ее натурального числа n надо посчитать сумму всех чисел,
+    /// на которые n делится без остатка. Маша не очень любит арифметику, и попросила вас написать программу, которая автоматизирует процесс гадания.
+    /// 
+    /// Входные данные
+    /// В единственной строке входного файла INPUT.TXT записано натуральное число n (n ≤ 1000), которое Маша была вынуждена сообщить.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT выведите сумму всех натуральных делителей числа n.
+    /// </summary>
+    public class Task0023
+    {
+        public static void Main()
+        {
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int n)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        public static int Solve(int n)
+        {
+            var sum = 0;
+            
+            for (var i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.Write(result);
+        }
+    }
+}
