fc322c679363ed6498af3d7d8c52f87e720562c1
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 01:42:43 +0300

    Решена задача 312 - "Арифметическая прогрессия" (Время: 1 сек. Память: 16 Мб Сложность: 15%)

---
+++ CSharp/TasksApp.Test/Tests/Task0312Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0312Test
+    {
+        private static void CheckTest(int a1, int a2, int n, int result)
+        {
+            var solution = Task0312.Solve(a1, a2, n);
+
+            Assert.AreEqual(result, solution);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(1, 5, 3, 9);
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0312.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 312 - "Арифметическая прогрессия"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 15%)
+    /// 
+    /// Заданы первый и второй элементы арифметической прогрессии.
+    /// Требуется написать программу, которая вычислит элемент прогрессии по ее номеру.
+    /// 
+    /// Входные данные
+    /// Первая строка входного файла INPUT.TXT содержит три целых числа,
+    /// разделенных пробелами – первый элемент прогрессии A(1) (1 ≤ A(1) ≤ 1000), второй элемент прогрессии A(2) (1 ≤ A(2) ≤ 1000), и номер требуемого элемента N (1 ≤ N ≤ 1000).
+    /// 
+    /// Выходные данные
+    /// Выходной файл OUTPUT.TXT должен содержать одно целое число - N-й элемент арифметической прогрессии.
+    /// </summary>
+    public static class Task0312
+    {
+        public static void Main()
+        {
+            GetInputData(out var a1, out var a2, out var n);
+
+            var result = Solve(a1, a2, n);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out int a1, out int a2, out int n)
+        {
+            var input = Console.ReadLine().Split();
+
+            a1 = int.Parse(input[0]);
+            a2 = int.Parse(input[1]);
+            n = int.Parse(input[2]);
+        }
+
+        public static int Solve(int a1, int a2, int n)
+        {
+            return a1 + (a2 - a1) * (n - 1);
+        }
+
+        private static void PrintResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
