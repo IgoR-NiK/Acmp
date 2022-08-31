41c68f235d313eb71f9ddfe0db176b3b82eb966b
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 02:32:52 +0300

    Решена задача 66 - "Клавиатура" (Время: 1 сек. Память: 16 Мб Сложность: 11%)

---
+++ CSharp/TasksApp.Test/Tests/Task0066Test.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0066Test
+    {
+        private static void CheckTest(char s, char result)
+        {
+            var resultTest = Task0066.Solve(s);
+            Assert.AreEqual(result, resultTest);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest('q', 'w');
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest('t', 'y');
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest('p', 'a');
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            CheckTest('l', 'z');
+        }
+        
+        [Test]
+        public void Test5()
+        {
+            CheckTest('m', 'q');
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0066.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 66 - "Клавиатура"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 11%)
+    /// 
+    /// Для данной буквы английского алфавита нужно вывести справа стоящую букву на стандартной клавиатуре.
+    /// При этом клавиатура замкнута, т.е. справа от буквы «p» стоит буква «a», от буквы «l» стоит буква «z», а от буквы «m» — буква «q».
+    /// 
+    /// Входные данные
+    /// Первая строка входного файла INPUT.TXT содержит один символ — маленькую букву английского алфавита.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT следует вывести букву стоящую справа от заданной буквы, с учетом замкнутости клавиатуры.
+    /// </summary>
+    public static class Task0066 
+    {
+        public static void Main()
+        {
+            GetInputData(out var symbol);
+
+            var result = Solve(symbol);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out char symbol)
+        {
+            symbol = (char)Console.Read();
+        }
+
+        public static char Solve(char symbol)
+        {
+            switch (symbol)
+            {
+                case 'q': return 'w';
+                case 'w': return 'e';
+                case 'e': return 'r';
+                case 'r': return 't';
+                case 't': return 'y';
+                case 'y': return 'u';
+                case 'u': return 'i';
+                case 'i': return 'o';
+                case 'o': return 'p';
+                case 'p': return 'a';
+                case 'a': return 's';
+                case 's': return 'd';
+                case 'd': return 'f';
+                case 'f': return 'g';
+                case 'g': return 'h';
+                case 'h': return 'j';
+                case 'j': return 'k';
+                case 'k': return 'l';
+                case 'l': return 'z';
+                case 'z': return 'x';
+                case 'x': return 'c';
+                case 'c': return 'v';
+                case 'v': return 'b';
+                case 'b': return 'n';
+                case 'n': return 'm';
+                default: return 'q';
+            }
+        }
+
+        private static void PrintResult(char result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
