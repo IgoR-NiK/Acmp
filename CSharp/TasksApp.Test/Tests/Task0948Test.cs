1c30ebeea4e4183948bcd101ba45d38f6785dcac
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-02 00:59:06 +0300

    Решена задача 948 - "Строки в книге" (Время: 1 сек. Память: 16 Мб Сложность: 13%)

---
+++ CSharp/TasksApp.Test/Tests/Task0948Test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0948Test
+    {
+        private static void CheckTest(int k, int n, Task0948.Result result)
+        {
+            var test = Task0948.Solve(k, n);
+			Assert.AreEqual(test, result);
+        }
+
+        [Test]
+        public void Test1()
+        {
+			CheckTest(50, 1, new Task0948.Result(1, 1));
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(20, 25, new Task0948.Result(2, 5));
+		}
+		
+		[Test]
+		public void Test3()
+		{
+			CheckTest(15, 43, new Task0948.Result(3, 13));
+		}
+		
+		[Test]
+		public void Test4()
+		{
+			CheckTest(15, 15, new Task0948.Result(1, 15));
+		}
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0948.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+	/// Задача 948 - "Строки в книге"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 13%)
+    /// 
+    /// В книге на одной странице помещается K строк. Таким образом, на 1-й странице печатаются строки с 1-й по K-ю,
+    /// на второй — с (K+1)-й по (2∙K)-ю и т.д. Напишите программу, которая по номеру строки в тексте определяет номер страницы,
+    /// на которой будет напечатана эта строка, и порядковый номер этой строки на странице.
+    /// 
+    /// Входные данные
+	/// Первая строка входного файла INPUT.TXT содержит два числа через пробел:
+	/// K – количество строк, которое печатается на странице, и число N – номер строки (1 ≤ K ≤ 200, 1 ≤ N ≤ 20000).
+    /// 
+    /// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите два числа – номер страницы, на которой будет напечатана эта строка и номер строки на странице.
+    /// </summary>
+	public class Task0948
+	{
+		public static void Main()
+		{
+			GetInputData(out var k, out var n);
+
+			var result = Solve(k, n);
+
+			PrintResult(result);
+		}
+
+		private static void GetInputData(out int k, out int n)
+		{
+			var input = Console.ReadLine().Split();
+
+			k = int.Parse(input[0]);
+			n = int.Parse(input[1]);
+		}
+
+		public static Result Solve(int k, int n) => new Result
+		{
+			Page = n % k == 0 ? n / k : n / k + 1,
+			Row = n % k == 0 ? k : n % k
+		};
+
+		private static void PrintResult(Result result)
+		{
+			Console.WriteLine(result);
+		}
+
+		public struct Result
+		{
+			public int Page { get; set; }
+			
+			public int Row { get; set; }
+
+			public Result(int page, int row)
+			{
+				Page = page;
+				Row = row;
+			}
+
+			public override string ToString() => $"{Page} {Row}";
+		}
+	}
+}
