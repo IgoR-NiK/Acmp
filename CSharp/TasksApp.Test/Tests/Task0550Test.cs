648dc0bf4e2158e64751d852e79ef5c99493bca0
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-02 00:34:32 +0300

    Решена задача 550 - "День программиста" (Время: 1 сек. Память: 16 Мб Сложность: 13%)

---
+++ CSharp/TasksApp.Test/Tests/Task0550Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0550Test
+    {
+        private static void CheckTest(int n, DateTime result)
+        {
+            var resultTest = Task0550.Solve(n);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(2000, new DateTime(2000, 09, 12));
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(2009, new DateTime(2009, 09, 13));
+		}
+	}
+}
---
+++ CSharp/TasksApp/Tasks/Task0550.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 550 - "День программиста"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 13%)
+	/// 
+	/// День программиста отмечается в 255-й день года (при этом 1 января считается нулевым днем).
+	/// Требуется написать программу, которая определит дату (месяц и число григорианского календаря), на которую приходится День программиста в заданном году.
+	///
+	/// В григорианском календаре високосным является:
+	/// год, номер которого делится нацело на 400
+	/// год, номер которого делится на 4, но не делится на 100
+	/// 
+	/// Входные данные
+	/// В единственной строке входного файла INPUT.TXT записано целое число от 1 до 9999 включительно, которое обозначает номер года нашей эры.
+	/// 
+	/// Выходные данные
+	/// В единственную строку выходного файла OUTPUT.TXT нужно вывести дату Дня программиста в формате DD/MM/YYYY,
+	/// где DD — число, MM — номер месяца (01 — январь, 02 — февраль, ..., 12 — декабрь), YYYY — год в десятичной записи.
+	/// </summary>
+	public static class Task0550 
+    {
+        public static void Main()
+        {            
+            GetInputData(out var n);
+
+            var result = Solve(n);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out int n)
+        {
+			n = int.Parse(Console.ReadLine());
+        }
+
+        public static DateTime Solve(int n)
+        {
+	        return new DateTime(n, 1, 1).AddDays(255);
+        }
+
+        private static void PrintResult(DateTime result)
+        {
+            Console.WriteLine(result.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+        }
+    }
+}
