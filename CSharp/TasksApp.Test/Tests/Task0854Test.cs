9c13ab44298fd1562c428dea462b33a19d0979fe
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-02 00:04:23 +0300

    Решена задача 854 - "Кондиционер" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task0854Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0854Test
+    {
+        private static void CheckTest(int a, int b, string mode, int result)
+        {
+            var resultTest = Task0854.Solve(a, b, mode);
+            Assert.AreEqual(resultTest, result);
+		}
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(10, 20, "heat", 20);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(10, 20, "freeze", 10);
+        }
+	}
+}
