d291ae91f929a8daa8ca42e9b34f00de8527cab7
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 22:20:31 +0300

    Решена задача 757 - "Спирт" (Время: 1 сек. Память: 16 Мб Сложность: 10%)

---
+++ CSharp/TasksApp.Test/Tests/Task0757Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0757Test
+    {
+        private static void CheckTest(long c, long h, long o, long result)
+        {
+            var resultTest = Task0757.Solve(c, h, o);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(2, 6, 1, 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+			CheckTest(10, 5, 12, 0);
+		}
+        
+        [Test]
+        public void Test3()
+        {
+            CheckTest(18, 35, 3, 3);
+        }
+	}
+}
---
+++ CSharp/TasksApp/Tasks/Task0757.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TasksApp.Tasks
+{
+	/// <summary>
+	/// Задача 757 - "Спирт"
+	/// (Время: 1 сек. Память: 16 Мб Сложность: 10%)
+	/// 
+	/// Каждому школьнику из курса органической химии известна формула молекулы этилового спирта – C(2)H(5)(OH).
+	/// Откуда видно, что молекула спирта состоит из двух атомов углерода (C), шести атомов водорода (H) и одного атома кислорода (O).
+	///
+	/// По заданному количеству атомов каждого из описанных выше элементов требуется определить максимально возможное
+	/// количество молекул спирта, которые могут образоваться в процессе их соединения.
+	/// 
+	/// Входные данные
+	/// Первая строка входного файла INPUT.TXT содержит 3 натуральных числа: C, Н и O – количество атомов углерода,
+	/// водорода и кислорода соответственно. Все числа разделены пробелом и не превосходят 10^18.
+	/// 
+	/// Выходные данные
+	/// В выходной файл OUTPUT.TXT выведите максимально возможное число молекул спирта, которые могут получиться из атомов, представленных во входных данных.
+	/// </summary>
+	public static class Task0757
+    {
+        public static void Main()
+        {            
+            GetInputData(out var c, out var h, out var o);
+
+            var result = Solve(c, h, o);
+
+            PrintResult(result);  
+        }
+
+        private static void GetInputData(out long c, out long h, out long o)
+        {
+            var inputData = Console.ReadLine().Split();
+            c = long.Parse(inputData[0]);
+            h = long.Parse(inputData[1]);
+            o = long.Parse(inputData[2]);
+		}
+
+        public static long Solve(long c, long h, long o)
+        {
+	        return Math.Min(Math.Min(c / 2, h / 6), o);
+        }
+
+        private static void PrintResult(long result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
