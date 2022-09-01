2d6e184b51e6c710534f6fa55ac504fe2f691df4
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-02 00:45:49 +0300

    Решена задача 940 - "Орфография" (Время: 1 сек. Память: 16 Мб Сложность: 13%)

---
+++ CSharp/TasksApp.Test/Tests/Task0940Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0940Test
+    {
+        private static void CheckTest(int k, string word, string result)
+        {
+            var test = Task0940.Solve(k, word);
+			Assert.AreEqual(test, result);
+        }
+
+        [Test]
+        public void Test1()
+        {
+			CheckTest(4, "MISTSPELL", "MISSPELL");
+        }
+
+		[Test]
+		public void Test2()
+		{
+			CheckTest(2, "ABC", "AC");
+		}
+    }
+}
