b096bc59f1042a75c61e286ec951b267ab9f0361
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 23:43:19 +0300

    Решена задача 777 - "Будильник" (Время: 1 сек. Память: 16 Мб Сложность: 12%)

---
+++ CSharp/TasksApp.Test/Tests/Task0777Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0777Test
+    {
+        private static void CheckTest(int s, int t, int result)
+        {
+            var resultTest = Task0777.Solve(s, t);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(10, 12, 2);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest(12, 10, 10);
+        }
+    }
+}
