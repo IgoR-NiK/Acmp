52a1f032e2301a14a8bcad016dcaa92a5b14ac34
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-02 01:06:45 +0300

    Решена задача 839 - "Всем известно" (Время: 1 сек. Память: 16 Мб Сложность: 14%)

---
+++ CSharp/TasksApp.Test/Tests/Task0839Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0839Test
+    {
+        private static void CheckTest(string input, bool result)
+        {
+            var resultTest = Task0839.Solve(input);
+            Assert.AreEqual(resultTest, result);
+		}
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest("11111101010011", false);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest("11", true);
+        }
+	}
+}
