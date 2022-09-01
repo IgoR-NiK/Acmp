5c13da9c6bace559bfb043479a72e1cba6132d29
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-02 01:32:10 +0300

    Решена задача 818 - "Кипячение чая" (Время: 1 сек. Память: 16 Мб Сложность: 14%)

---
+++ CSharp/TasksApp.Test/Tests/Task0818Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0818Test
+    {
+        private static void CheckTest(int[] numbers, int result)
+        {
+            var resultTest = Task0818.Solve(numbers);
+            Assert.AreEqual(resultTest, result);
+		}
+
+        [Test]
+        public void Test1()
+        {
+	        CheckTest(new[] { 1 }, 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+	        CheckTest(new[] { 2, 5, 4 }, 9);
+        }
+	}
+}
