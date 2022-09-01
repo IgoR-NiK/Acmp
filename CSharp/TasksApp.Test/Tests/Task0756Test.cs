369662cecf0838059d02579af414abda69c92557
Igor Nikolaev <IgoR_2501@mail.ru>
2022-09-01 22:10:43 +0300

    Решена задача 756 - "Рыболовная сеть" (Время: 1 сек. Память: 16 Мб Сложность: 10%)

---
+++ CSharp/TasksApp.Test/Tests/Task0756Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0756Test
+    {
+        private static void CheckTest(int m, int n, int result)
+        {
+            var resultTest = Task0756.Solve(m, n);
+            Assert.AreEqual(resultTest, result);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest(2, 2, 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+			CheckTest(2, 3, 2);
+		}
+	}
+}
