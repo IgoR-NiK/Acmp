de1e8b6ee26829fb41ddd7aaa17a0b6d2099bf89
Igor Nikolaev <IgoR_2501@mail.ru>
2023-07-19 13:12:43 +0300

    Решена задача 346 - "Сумма двух чисел" (Время: 1 сек. Память: 16 Мб Сложность: 46%)

---
+++ CSharp/TasksApp.Test/Tests/Task0346Test.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using TasksApp.Tasks;
+
+namespace TasksApp.Test.Tests
+{
+    [TestFixture]
+    public class Task0346Test
+    {
+        private static void CheckTest(string a, string b, int c, Task0346.Result result)
+        {
+            var actual = Task0346.Solve(a, b, c);
+            
+            Assert.AreEqual(result.Answer, actual.Answer);
+            Assert.AreEqual(result.X, actual.X);
+            Assert.AreEqual(result.Y, actual.Y);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            CheckTest("12", "31", 25, new Task0346.Result { Answer = true, X = 12, Y = 13 });
+        }
+
+        [Test]
+        public void Test2()
+        {
+            CheckTest("12", "31", 26, new Task0346.Result { Answer = false });
+        }
+
+        [Test]
+        public void Test3()
+        {
+            CheckTest("101", "2", 13, new Task0346.Result { Answer = true, X = 11, Y = 2 });
+        }
+
+        [Test]
+        public void Test4()
+        {
+            CheckTest("54321", "9876", 19134, new Task0346.Result { Answer = true, X = 12345, Y = 6789 });
+        }
+
+        [Test]
+        public void Test5()
+        {
+            CheckTest("1", "10", 2, new Task0346.Result { Answer = true, X = 1, Y = 1 });
+        }
+
+        [Test]
+        public void Test6()
+        {
+            CheckTest("10", "1000", 11, new Task0346.Result { Answer = true, X = 1, Y = 10 });
+        }
+
+        [Test]
+        public void Test7()
+        {
+            CheckTest("13", "101", 24, new Task0346.Result { Answer = true, X = 13, Y = 11 });
+        }
+
+        [Test]
+        public void Test8()
+        {
+            CheckTest("11", "11", 121, new Task0346.Result { Answer = false });
+        }
+
+        [Test]
+        public void Test9()
+        {
+            CheckTest("987654321", "1", 123456790, new Task0346.Result { Answer = true, X = 123456789, Y = 1 });
+        }
+
+        [Test]
+        public void Test10()
+        {
+            CheckTest("122", "1", 222, new Task0346.Result { Answer = true, X = 221, Y = 1 });
+        }
+
+        [Test]
+        public void Test11()
+        {
+            CheckTest("666", "100", 667, new Task0346.Result { Answer = true, X = 666, Y = 1 });
+        }
+
+        [Test]
+        public void Test12()
+        {
+            CheckTest("100", "666", 667, new Task0346.Result { Answer = true, X = 1, Y = 666 });
+        }
+
+        [Test]
+        public void Test13()
+        {
+            CheckTest("12345", "1", 54322, new Task0346.Result { Answer = true, X = 54321, Y = 1 });
+        }
+
+        [Test]
+        public void Test14()
+        {
+            CheckTest("351", "0", 15, new Task0346.Result { Answer = false });
+        }
+    }
+}
---
+++ CSharp/TasksApp/Tasks/Task0346.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksApp.Tasks
+{
+    /// <summary>
+    /// Задача 346 - "Сумма двух чисел"
+    /// (Время: 1 сек. Память: 16 Мб Сложность: 46%)
+    /// 
+    /// Заданы три числа: a, b, c. Необходимо выяснить, можно ли так переставить цифры в числах a и b, чтобы в сумме получилось c.
+    /// 
+    /// Входные данные
+    /// Входной файл INPUT.TXT содержит три целых числа: a, b, c (0 ≤ a, b, c < 10^9). Числа разделены пробелом.
+    /// 
+    /// Выходные данные
+    /// В выходной файл OUTPUT.TXT следует вывести YES, если искомая перестановка цифр возможна, 
+    /// в противном случае необходимо вывести NO. При положительном ответе во второй строке следует вывести число x, 
+    /// получаемое перестановкой цифр числа a, и число y, получаемое перестановкой цифр числа b, сумма которых равна c. 
+    /// Числа x и y при выводе не должны содержать ведущих нулей. Числа в строке разделены пробелом. 
+    /// Если решений несколько, то следует вывести ту пару, в которой число x минимально.
+    /// </summary>
+    public class Task0346
+    {
+        public static void Main()
+        {
+            GetInputData(out var a, out var b, out var c);
+
+            var result = Solve(a, b, c);
+
+            PrintResult(result);
+        }
+
+        private static void GetInputData(out string a, out string b, out int c)
+        {
+            var numbers = Console.ReadLine().Split();
+
+            a = numbers[0];
+            b = numbers[1];
+            c = int.Parse(numbers[2]);
+        }
+
+        public static Result Solve(string a, string b, int c)
+        {
+            var MassX = Swap((a.Length < b.Length) ? a : b);
+
+            MassX.Sort();
+
+            for (var i = 0; i < MassX.Count; i++)
+            {
+                var j = (a.Length < b.Length) ? i : MassX.Count - i - 1;
+                int r = c - MassX[j];
+
+                if (r < 0)
+                {
+                    if (a.Length < b.Length)
+                    {
+                        return new Result { Answer = false };
+                    }
+
+                    continue;
+                }
+
+                if (CheckDigit((a.Length < b.Length) ? b : a, r.ToString()))
+                {
+                    return new Result 
+                    { 
+                        Answer = true, 
+                        X = a.Length < b.Length ? MassX[j] : r, 
+                        Y = a.Length < b.Length ? r : MassX[j] 
+                    };
+                }
+            }
+
+            return new Result { Answer = false };
+        }
+
+        private static void PrintResult(Result result)
+        {
+            Console.WriteLine(result);          
+        }
+
+        private static bool CheckDigit(string a, string b)
+        {
+            var d = new int[10];
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                d[a[i] - '0']++;
+            }
+
+            for (var i = 0; i < b.Length; i++)
+            {
+                d[b[i] - '0']--;
+            }
+
+            return d[0] >= 0 && d[1] == 0 && d[2] == 0 && d[3] == 0 && d[4] == 0 && d[5] == 0 && d[6] == 0 && d[7] == 0 && d[8] == 0 && d[9] == 0;
+        }
+
+        private static List<int> Swap(string s)
+        {
+            switch (s.Length)
+            {
+                case 1: return new List<int> { int.Parse(s) };
+                case 2: return new List<int> { int.Parse(s), int.Parse($"{s[1]}{s[0]}") };
+                default: return Combination(s[0] - '0', Swap(s.Substring(1)));
+            }
+        }
+
+        private static List<int> Combination(int digit, List<int> numbers)
+        {
+            var results = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                int discharge = 1, whole;
+                do
+                {
+                    whole = number / discharge;
+                    results.Add(((whole * 10) + digit) * discharge + number % discharge);
+                    discharge *= 10;
+                } while (whole != 0);
+            }
+            
+            return results;
+        }
+
+        public class Result
+        {
+            public bool Answer { get; set; }
+
+            public int X { get; set; }
+
+            public int Y { get; set; }
+
+            public override string ToString() => Answer ? $"YES{Environment.NewLine}{X} {Y}" : "NO";
+        }
+    }
+}
